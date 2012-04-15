2d915b0afee1b981b29edf619486cfc034a66224
Heinrich Hördegen <hoerdegen@funktional.info>
2012-04-15 15:27:00 +0200

    fixed solver test

--- src/EFA2/Test/SolverTest.hs
+++ src/EFA2/Test/SolverTest.hs
@@ -26,11 +26,11 @@ prop_solver :: Int -> Double -> Bool
 prop_solver numOfNodes ratio | numOfNodes > 1 && numOfNodes < 300 && ratio > 2.0 && ratio < 5.0 = length dirs == 2*(length $ edges g)
   where seed = 12
         g = randomTopology seed numOfNodes ratio
-        terms = [ PowerIdx 0 1 .= [2.2 :: Val] ]
+        terms = [ PowerIdx 0 0 0 1 .= [2.2 :: Val] ]
         xenvts = envToEqTerms (randomXEnv 0 0 g)
         eenvts = envToEqTerms (randomEtaEnv 17 0 g)
 
-        ts = terms ++ xenvts ++ eenvts ++ mkEdgeEq g ++ mkNodeEq g
+        ts = terms ++ xenvts ++ eenvts ++ mkEdgeEq 0 0 g ++ mkNodeEq 0 0 g
         ho = hornOrder ts
         dirs = directEquations ho
 prop_solver _ _ = True
