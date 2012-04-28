4ac15a839e8cc3a586925b89e4e5c24cf4e14ef1
Philipp <philipp.guttenberg@gmx.net>
2012-04-28 12:05:29 +0200

    First draft of Display is working

--- demo/typedArith/Main_new.hs
+++ demo/typedArith/Main_new.hs
@@ -5,6 +5,9 @@ import qualified Data.Vector as GV
 import qualified Data.Vector.Generic.Mutable as MV
 import qualified Data.Foldable as F
 
+import qualified Data.Map as M
+import Text.Printf
+
 -----------------------------------------------------------------------------------
 -- Basic Data structures used 
 
@@ -19,31 +22,37 @@ type State = Bool
 -- Data Formats containing the data
   
 newtype Signal a = Signal a deriving (Show, Eq)
-newtype Dist a = Dist a deriving (Show, Eq)
+newtype Distrib a = Distrib a deriving (Show, Eq)
 newtype Value a = Value a  deriving (Show, Eq)
+newtype Curve a = Curve a  deriving (Show, Eq)
+
 
 --------------------------------------------------------------------------------------------
 -- Helpful Type Synoymns
-type VSig = Signal (GV.Vector Val)
-type USig = Signal (UV.Vector Val)
-type LSig = Signal [Val]
+type Sig = Signal (UV.Vector Val)
+-- type LSig = Signal [Val]
+-- type SignSig = Signal (GV.Vector Sign)
 
-type VBSig = Signal (GV.Vector Bool)
-type UBSig = Signal (UV.Vector Bool)
-type LBSig = Signal [Bool]
 
-type VISig = Signal (GV.Vector Int)
-type UISig = Signal (UV.Vector Int)
-type LISig = Signal [Int]
+type BSig = Signal (UV.Vector Bool)
+-- type LBSig = Signal [Bool]
+
+type ISig = Signal (UV.Vector Int)
+-- type LISig = Signal [Int]
+
+
 
 type DVal = Value Double
 type BVal = Value Bool
 type IVal = Value Int
 
+type Dist = Distrib (UV.Vector Val)
+
+type Cur = Curve (UV.Vector Val) 
 
 -- ?? Do we need a signal for user defined types here as well ?? --
 -- type VISig a = (Num a) ... 
-
+-- no hand-code specifically
 
 --------------------------------------------------------------------------------------------
 -- Data Container Type
@@ -54,40 +63,46 @@ class Data (cont :: * -> *) b where
    toData :: a -> cont a
   
 instance Data Signal b where fromData (Signal x) = x; toData x = Signal x                               
-instance Data Dist  b where fromData (Dist x) = x; toData x = Dist x                               
+instance Data Distrib  b where fromData (Distrib x) = x; toData x = Distrib x                               
 instance Data Value  b where fromData (Value x) = x; toData x = Value x                               
-                                  
+instance Data Curve  b where fromData (Curve x) = x; toData x = Curve x                               
 
 -- Sample Calculation Class
 class DProd a b c | a b -> c , b c -> a where
   (.*) :: a -> b -> c
   (./) :: c -> b -> a
   
-instance DProd USig USig USig where
+instance DProd Sig Sig Sig where
   (.*) x y = toData (UV.zipWith (*) (fromData x) (fromData y))
-  (./) x y = toData (UV.zipWith (*) (fromData x) (fromData y))
+  (./) x y = toData (UV.zipWith (/) (fromData x) (fromData y))
 
-instance DProd VSig VSig VSig where
-  (.*) x y = toData (GV.zipWith (*) (fromData x) (fromData y))
-  (./) x y = toData (GV.zipWith (*) (fromData x) (fromData y))
+-- instance DProd VSig VSig VSig where
+--   (.*) x y = toData (GV.zipWith (*) (fromData x) (fromData y))
+--   (./) x y = toData (GV.zipWith (/) (fromData x) (fromData y))
 
-instance DProd USig DVal USig where
+instance DProd Sig DVal Sig where
   (.*) x y = toData (UV.map (*(fromData y)) (fromData x))
-  (./) x y = toData (UV.map (*(fromData y)) (fromData x))
+  (./) x y = toData (UV.map (/(fromData y)) (fromData x))
 
+-- Generating Curves & Calculating with curves 
+
+instance DProd Dist Cur Dist where
+  (.*) x y = toData (UV.zipWith (*) (fromData x) (fromData y))
+  (./) x y = toData (UV.zipWith (/) (fromData x) (fromData y))
+
+{- use interp1 here to lookup efficiency in curve
+instance DProd USig Curve USig  where
+  (.*) x y = toData (GV.zipWith interp1 (fromData x) (fromData y))
+  (./) x y = toData (GV.zipWith (*) (fromData x) (fromData y))
+-}
 
 class DSum a b c | a b -> c , b c -> a where
   (.+) :: a -> b -> c
   (.-) :: c -> b -> a
   
-instance DSum USig USig USig where
+instance DSum Sig Sig Sig where
   (.+) x y = toData (UV.zipWith (+) (fromData x) (fromData y))
   (.-) x y = toData (UV.zipWith (-) (fromData x) (fromData y))
-
-instance DSum VSig VSig VSig where
-  (.+) x y = toData (GV.zipWith (+) (fromData x) (fromData y))
-  (.-) x y = toData (GV.zipWith (-) (fromData x) (fromData y))
-
 
 --------------------------------------------------------------------------------------------
 -- Data Sample Type
@@ -213,6 +228,19 @@ instance  DSum a b c => TSum DN a DN  b DN c where
 instance  DSum a b c => TSum DT a DT  b DT c where 
 instance  DSum a b c => TSum DX a DX  b DX c where 
 
+-- =================== Data   
+   
+{-  
+class DataInfo a b where 
+  dlength:: a -> b 
+  
+  
+instance DataInfo Sig where  
+  dlength (Signal v) = UV.length v  
+  dmaximum (Signal v)= UV.maxiumum v 
+  dminimum (Signal v)= UV.maxiumum v 
+-}  
+    
 {-  
 -- Sub Class  
 class   (Type t1 a, Type t2 b, Type t3 c, DSum a b c)  => TSub t1 a t2 b t3 c | t1 t2 -> t3  where 
@@ -227,7 +255,93 @@ instance  DSum a b c => TSub N a N  b DN c where
 instance  DSum a b c => TSub T a T  b DT c where 
 -} 
 
+-- ############################ Display #############################
+  
+class (Show a, PrintfArg a,Fractional a) => TShow a
 
+data DispLength = Short | Middle | Long | Float 
+
+
+
+class Disp a where 
+  disp :: a -> String -> Val -> String
+
+instance Disp Val where
+  disp 0 _ _ = "Null"
+  disp x format scale = printf format $ x*scale
+
+instance Disp Bool where
+  disp x format scale = printf format (show x)
+
+instance Disp Int where
+  disp x format scale = printf format (show x)
+
+-- | choose a Form of Display
+dispLength = Short
+
+-- | Map to choose display Unit per type
+dispUnitMap = M.fromList [("P","kW"),
+                      ("E","kWh"),
+                      ("X","%"),
+                      ("M","%"),
+                      ("N","/"), 
+                      ("T","s")]; 
+
+-- | Map to lookup display scale per Unit 
+unitMap = M.fromList [("kW",1/1000),
+                      ("kWh",1/1000/3600),
+                      ("%",100),
+                      ("s",1),
+                      ("/",1)];
+
+-- | Map to choose displayFormat for a Kombination of Type and Unit -- three Options -- short, middle, long, float                 
+displayFormatMap = M.fromList [(("P","kW"),("%6.7f","%6.7f","%6.7f","%6.7f")),
+                               (("E","kWh"),("%6.7f","%6.7f","%6.7f","%6.7f")),
+                               (("T","s"),("%6.7f","%6.7f","%6.7f","%6.7f")),
+                               (("X","%"),("%6.7f","%6.7f","%6.7f","%6.7f")),
+                               (("N","%"),("%6.7f","%6.7f","%6.7f","%6.7f")),
+                               (("M","/"),("%6.7f","%6.7f","%6.7f","%6.7f"))]
+
+formatSelect :: DispLength -> (String,String,String,String) -> String
+formatSelect Short (f1,f2,f3,f4) = f1
+formatSelect Middle (f1,f2,f3,f4) = f2
+formatSelect Long (f1,f2,f3,f4) = f3
+formatSelect Float (f1,f2,f3,f4) = f4
+
+  
+dispSingle :: Disp a => a -> String -> String
+dispSingle x typ = disp x format scale ++ " " ++ dunit
+           where dunit = dispUnitMap M.! typ
+                 scale = unitMap M.! dunit
+                 format = formatSelect dispLength $ displayFormatMap M.! (typ,dunit)
+
+dispRange :: Disp a => a -> a -> String -> String
+dispRange x y typ = disp x format scale ++ "-" ++ disp y format scale ++ " " ++ dunit
+           where dunit = dispUnitMap M.! typ
+                 scale = unitMap M.! dunit
+                 format = formatSelect dispLength $ displayFormatMap M.! (typ,dunit)
+  
+                               
+class DataDisplay a where
+  ddisp :: a -> (String) -> String
+          
+instance DataDisplay Sig  where 
+  ddisp (Signal v) (typString) = dispRange (UV.minimum v) (UV.maximum v) typString
+
+
+class TypedDisplay (typ :: * -> *) a where
+  tdisp :: typ a -> String
+
+instance (DataDisplay a) => TypedDisplay P a where
+  tdisp (P d) = ddisp d "P" 
+  
+instance (DataDisplay a) => TypedDisplay E a where
+  tdisp (E d) = ddisp d "E" 
+
+  
+  
+  
+  
 s1 = Signal (UV.fromList [0.5,0.3::Val]) 
 s2 = Signal (UV.fromList [0.8,0.2::Val])
 v = Value 2 :: Value Val
@@ -245,6 +359,7 @@ main = do
   putStrLn $ show (s)
   putStrLn $ show (s')
   putStrLn $ show (e)
+  putStrLn $ tdisp (e)
   
   
 --  putStrLn $ show (d.*s2)
