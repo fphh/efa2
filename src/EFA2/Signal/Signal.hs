a3c0b4a3d779e1f2266d8265fbe3a93c47ceb7cb
Philipp Guttenberg <philipp.guttenberg@gmx.net>
2012-05-04 08:33:34 +0200

    Failed Attempt to get rid of TC and make newType Signal, ...

--- src/EFA2/Signal/Signal.hs
+++ src/EFA2/Signal/Signal.hs
@@ -24,8 +24,14 @@ class TMult t1 t2 t3 | t1 t2 -> t2 where
 instance TMult P DT E
 
 
-
-
+-- class SMult s1 s2 s3 | s1 s2 -> s3 where
+-- instance SMult Signal Signal Signal
+-- instance SMult Signal Scalar Signal
+-- instance SMult s1 s2 s3 => SMult s2 s1 s3 
+  
+newtype Signal t c d = Signal (c d) deriving Show
+newtype Scalar t c d = Scalar (c d) deriving Show
+newtype Distrib t c d = Distrib (c d) deriving Show
 
 ----------------------------------------------------------
 -- | 2. EFA data containers
@@ -35,33 +41,55 @@ instance TMult P DT E
 -- c = container
 
 
-newtype TC t c d = TC (c d) deriving (Show) 
+-- newtype TC t c d = TC (c d) deriving (Show) 
 
-class TCont a t c d where
-  fromTC ::  a t c d -> c d
-  toTC :: c d -> a t c d 
+class TCont s t c d where
+  fromTC ::  s t c d -> c d
+  toTC :: c d -> s t c d 
   
-instance TCont TC t c d where  
+instance TCont Signal t c d where  
+  fromTC (Signal x) = x 
+  toTC x = Signal x 
+
+instance TCont Scalar t c d where  
+  fromTC (Scalar x) = x 
+  toTC x = Scalar x 
+
+instance TCont Distrib t c d where  
+  fromTC (Distrib x) = x 
+  toTC x = Distrib x 
+
+class (TCont s1 t1 c1 d1, TCont s2 t2 c2 d2, TCont s3 t3 c3 d3, EZipWith c1 c2 c3 d1 d2 d3) => Apply2 s1 s2 s3 t1 t2 t3 c1 c2 c3 d1 d2 d3 where 
+      apply2EC ::  (c1 d1 -> c2 d2 -> c3 d3) -> s1 t1 c1 d1 -> s2 t2 c2 d2 -> s3 t3 c3 d3
+      apply2EC f x y = toTC $ f (fromTC x) (fromTC y)
+      
+instance (TCont s1 t1 c1 d1, TCont s2 t2 c2 d2, TCont s3 t3 c3 d3,Show (c1 d1), Show (c2 d2)) => Apply2 Signal Signal Signal t1 t2 t3 c1 c2 c3 d1 d2 d3
+      
+
+(~*) :: (TMult t1 t2 t3, DMult d1 d2 d3,Apply2 s1 s2 s3 t1 t2 t3 c1 c2 c3 d1 d2 d3,TCont s1 t1 c1 d1, TCont s2 t2 c2 d2, TCont s3 t3 c3 d3) => s1 t1 c1 d1 -> s2 t2 c2 d2 -> s3 t3 c3 d3 
+(~*) x y = apply2EC (.*) x y 
+
+
+
+-- ############### Geht Das ? #################
+{-
+newtype TC t e = TC e deriving (Show) 
+
+class TCont a t e where
+  fromTC ::  a t e -> e
+  toTC :: c d -> a t e
+  
+instance TCont TC t e where  
   fromTC (TC x) = x 
   toTC x = TC x 
 
 
 
-apply2 :: (TCont a1 t1 c1 d1, TCont a2 t2 c2 d2, TCont a3 t3 c3 d3)  => (c1 d1 -> c2 d2 -> c3 d3) -> a1 t1 c1 d1 -> a2 t2 c2 d2 -> a3 t3 c3 d3
+apply2 :: (TCont a1 t1 e1, TCont a2 t2 e2, TCont a3 t3 e3)  => (e1 -> e2 -> e3) -> a1 t1 e1 -> a2 t2 e2 -> a3 t3 e3
 apply2 f x y = toTC $ f (fromTC x) (fromTC y)
   
   
-(~*) :: (TMult t1 t2 t3, DMult d1 d2 d3, EZipWith c1 c2 c3 d1 d2 d3, Show (c1 d1), Show (c2 d2)) => TC t1 c1 d1 -> TC t2 c2 d2 -> TC t3 c3 d3 
-(~*) x y = apply2 (.*) x y 
-
-{-
-newtype TC t e = TC e deriving (Show) 
-
-class (EsipWith c1 c2 c3 d1 d2 d3) =>  Apply2 t1 t2 t3 e1 e2 e3 where 
-  apply2 :: (e1 -> e2 -> e3) -> TC t1 e1 -> TC t2 e2 -> TC t3 e3
-  apply2 f (TC x) (TC y) = TC $ f x y
-  
-  
-(~*) :: (TMult t1 t2 t3) => TC t1 e1 -> TC t2 e2 -> TC t3 e3
+-- (~*) :: TC t1 e1 -> TC t2 e2 -> TC t3 e3
 (~*) x y = apply2 (.*) x y 
 -}
+-- ############### Traumvorstellung #################
