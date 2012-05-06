9484e2e5a84e0fc2bd28bc8022c198cfea8a147f
Philipp Guttenberg <philipp.guttenberg@gmx.net>
2012-05-06 21:37:28 +0200

    Attempt of a Type switcher in Signal

--- demo/typedArith/Demo_Signal.hs
+++ demo/typedArith/Demo_Signal.hs
@@ -10,20 +10,24 @@ import qualified Data.Vector.Unboxed as UV
 v1 =  DC (UV.fromList [0..3]) :: (DC D1 (UVec Val))
 v2 =  DC (UV.fromList [0..3]) :: (DC D1 (UVec Val))
 
-s1 = TC v1 :: TC P Signal (DC D1 (UVec Val)) 
-s2 = TC v2 :: TC DT Signal (DC D1 (UVec Val)) 
+s1 = TC 1 :: TC P Signal Val -- v1 :: TC E Signal (DC D1 (UVec Val)) 
+s2 = TC 2 :: TC DT Signal Val -- v2 :: TC DT Signal (DC D1 (UVec Val)) 
+s3 = TC 3 :: TC DT SampleVec Val -- v2 :: TC DT SampleVec (DC D1 (UVec Val)) 
 
 v3 = v1 .* v2
 
-s3 = (apply2EC (.*) s1 s2)
-s4 = (s1 ~* s2) -- ::  TC E Signal (DC H (UVec Val))
+-- s3 = (apply2EC (.*) s1 s2)
+s4 = (s1 ~* s2) 
 
+s5 = (s1 ~* s3)   
+-- s6 = (s1 ~* s3)   
 
 main = do 
-  putStrLn (show s1) 
-  putStrLn (show s2) 
   
-  putStrLn (show v3) 
-  putStrLn (show s3)
---  putStrLn (show s4)
- 
+--  putStrLn (show s1) 
+--  putStrLn (show s2) 
+  
+--  putStrLn (show v3) 
+  putStrLn (show s4)
+  putStrLn (show s5)
+--  putStrLn (show s6) 
--- src/EFA2/Signal/Signal.hs
+++ src/EFA2/Signal/Signal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses,FunctionalDependencies, TypeSynonymInstances, UndecidableInstances,KindSignatures,IncoherentInstances, GeneralizedNewtypeDeriving,FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, TypeSynonymInstances, UndecidableInstances #-}
 
 module EFA2.Signal.Signal (module EFA2.Signal.Signal) where
 import EFA2.Signal.Vector2
@@ -10,65 +10,113 @@ newtype TC t s e = TC e deriving (Show)
 --------------------------------
 -- | Physical Types & Classification
 data P 
-data DT
+data DT 
 data E 
 data N
+data M
+data X
 data BZ 
 data IZ
 data UZ
 
 class TMult t1 t2 t3 | t1 t2 -> t3 where
-instance TMult P DT E
-instance TMult E N  E
-
-class  TDiv t1 t2 t3
-instance TMult t1 t2 t3 => TDiv t3 t2 t1 
-
-----------------------------------------------------------
--- | Position
-
--- data S
--- data H
--- data V
--- data H2
--- data V2
+    
+instance TMult N N N  
+instance TMult DT P E 
+instance TMult P DT E 
 
 
 ----------------------------------------------------------
 -- | EFA data containers
 
-data Scalar
-data Signal
+data Scalar 
+data Signal 
 data Distrib
 data FSignal
 data FDistrib
+data SampleVec
+data FSampleVec
+data ClassVec
 
-class SArith s1 s2 s3 | s1 s2 -> s3 where
-instance SArith Signal Signal Signal
-instance SArith Scalar Signal Signal
+data Norm
+data Cross
 
-instance SArith FSignal FSignal FSignal
-instance SArith Scalar FSignal FSignal
-
-instance SArith s1 s2 s3 => SArith s2 s1 s3 
+class Flag s1 s2 h1 h2 h3 | s1 s2 -> h1 h2 h3 where
   
+instance Flag Signal Signal Norm Norm Cross
+instance Flag Signal SampleVec Norm Cross Cross 
+
+-- Signal Aritmetics
+class SArith s1 s2 s3 | s1 s2 -> s3 where -- hv1 hv2 hv3
+--  foo :: s1 -> s2 -> s3 -> Int 
+  
+instance SArith Signal Signal Distrib 
+
+instance SArith Signal SampleVec Signal 
+{-
+instance SArith FSignal FSignal FSignal Norm Norm Cross 
+instance SArith FSampleVec FSignal FSignal Norm Cross Cross 
+
+instance SArith Scalar Signal Signal Norm Norm Cross 
+
+instance SArith Distrib Distrib Distrib Norm Norm Cross 
+instance SArith Distrib ClassVec Distrib  Norm Cross Cross 
+
+
+-}  
+-- instance SArith s1 s2 s3 hv1 hv2 hv3 => SArith s2 s1 s3 hv1 hv2 hv3
+
 ----------------------------------------------------------
 -- | Apply Function
 
-apply2EC :: (e1 -> e2 -> e3) -> TC t1 s1 e1 -> TC t2 s2 e2 -> TC t3 s3 e3
-apply2EC f (TC x) (TC y) = TC $ f x y
+-- apply2TC :: (e1 -> e2 -> e3) -> TC t1 s1 e1 -> TC t2 s2 e2 -> TC t3 s3 e3
+-- apply2TC f (TC x) (TC y) = TC $ f x y
   
+apply2TC :: (e1 -> e2 -> e3) -> TC t1 s1 e1 -> TC t2 s2 e2 -> TC t3 s3 e3
+apply2TC f (TC x) (TC y) = TC $ f x y
 
 
 
-class (DArith1 e1 e2 e3, SArith s1 s2 s3, TMult t1 t2 t3 ) => PhysArith t1 s1 e1 t2 s2 e2 t3 s3 e3  where
-  (~*) :: TC t1 s1 e1 -> TC t2 s2 e2 -> TC t1 s3 e3 
-  (~*) x y = apply2EC (.*) x y
+class  PhysArith h1 h2 h3 t1 t2 t3  s1 s2 s3 e1 | t1 t2 -> t3 where
+  (~*) :: TC t1 s1 e1 -> TC t2 s2 e1 -> TC t3 s3 e1 
 
-instance  (DArith1 e1 e2 e3, SArith s1 s2 s3, TMult t1 t2 t3) => PhysArith t1 s1 e1 t2 s2 e2 t3 s3 e3
+instance (SArith s1 s2 s3, TMult t1 t2 t3, Flag s1 s2 h1 h1 h3) => PhysArith h1 h1 h3 t1 t2 t3 s1 s2 s3 Val where
+  (~*) x y = apply2TC (*) x y
 
+instance (SArith s1 s2 s3, TMult t1 t2 t3, Flag s1 s2 h1 h2 h2) => PhysArith h1 h2 h2 t1 t2 t3 s1 s2 s3 Val where
+  (~*) x y = apply2TC (/) x y
 
+{-
+instance (SArith s1 s2 s3 Norm, TMult t1 t2 t3) => PhysArith Norm t1 t2 t3 s1 s2 s3 Val where
+  (~*) x y = apply2TC (*) x y
 
+instance (SArith s1 s2 s3 Cross, TMult t1 t2 t3) => PhysArith Cross t1 t2 t3 s1 s2 s3 Val where
+  (~*) x y = apply2TC (/) x y
+-}
+{-
+instance  ( SArith s1 s2 s3 hv1) => PhysArith h1 P DT E s1 s2 s3 (DC D1 (UVec Val)) (DC D1 (UVec Val)) (DC D1 (UVec Val)) where
+  (~*) x y = apply2TC (.*) x y
+-}
+-- instance  (DArith1 e1 e2 e3, SArith s1 s2 s3 Cross, TMult t1 t2 t3) => PhysArith Cross t1 t2 t3 s1 s2 s3 e1 e2 e3 where
+--   (~*) x y = apply2TC (./) x y
 
+-- instance  (PhysArith hv0 P DT t30 Signal Signal s30 (DC D1 (UVec Val)) (DC D1 (UVec Val)) e30) where
+--   (~*) x y = apply2TC (./) x y
 
+class Disp t s e where
+  disp :: TC t s e -> String
 
+instance Disp P s Val where
+  disp x = "P" 
+  
+-- instance Disp E s Val where  
+--   disp x = "E"
+
+instance Disp t SampleVec Val where  
+  disp x = "SampleVec"
+
+instance Disp t Signal Val where  
+  disp x = "Signal"
+
+instance Disp t Distrib Val where  
+  disp x = "Distrib"
