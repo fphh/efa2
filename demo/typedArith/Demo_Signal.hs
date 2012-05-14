866cb7821bf07f4d02614b900fcf03ee4a36ac9e
Philipp <philipp.guttenberg@gmx.net>
2012-05-14 12:05:55 +0200

    Typed Signal Arith works now

--- demo/typedArith/Demo_Signal.hs
+++ demo/typedArith/Demo_Signal.hs
@@ -19,16 +19,21 @@ v1 =  (UV.fromList [0..3])
 v2 =  (UV.fromList [0..3]) 
 
 p1 = TC $ Data $ D1 v1 :: TC (Typ A P) Signal (Data (UVec :> Nil) Val)
+p2 = TC $ Data $ D1 v1 :: TC (Typ A P) Signal (Data (UVec :> Nil) Val)
 dt = TC $ Data $ D1 v2 :: TC (Typ D T) Signal (Data (UVec :> Nil) Val)
-dp = TC $ Data $ D1 v2 :: TC (Typ D P) Signal (Data (UVec :> Nil) Val)
+-- dp = TC $ Data $ D1 v2 :: TC (Typ D P) Signal (Data (UVec :> Nil) Val)
 
 
 e = dt .* p1
-p2 = p1 .+ dp
+p3 = p1 .+ dp
+dp = p1 .- p2
+dt2 = e ./ p2
 
 
 
 main = do 
   
   putStrLn (show e)
-  putStrLn (show p2)
+  putStrLn (show p3)
+  putStrLn (show dp)
+  putStrLn (show dt)
--- src/EFA2/Signal/Signal.hs
+++ src/EFA2/Signal/Signal.hs
@@ -22,7 +22,7 @@ data FSample
 data FDistrib
 data FClass
 
--- Product
+-- Rule of Signal Inheritance
 class SArith s1 s2 s3 | s1 s2 -> s3
 
 instance SArith Scalar Scalar Scalar
@@ -41,25 +41,20 @@ instance SArith FClass FClass FClass
 
   
 ----------------------------------------------------------
--- Signal Aritmetics
+-- Signal Arithmetics
 
 class Prod t1 t2 t3 s1 s2 s3 c1 c2 c3 | s1 s2 -> s3, c1 c2 -> c3   where
   (.*) :: TC t1 s1 c1 -> TC t2 s2 c2 -> TC t3 s3 c3
---  (./) :: s1 -> s2 -> s3
---  (.+) :: s1 -> s2 -> s3
---  (.-) :: s1 -> s2 -> s3
+  (./) :: TC t3 s1 c1 -> TC t2 s2 c2 -> TC t1 s3 c3
      
 instance  (DZipWith v1 v2 d1 d2 d3, DArith d1 d2 d3, SArith s1 s2 s3, TProd t1 t2 t3) =>  Prod t1 t2 t3 s1 s2 s3 (v1 d1) (v2 d2) (v2 d3) where
           (.*) (TC x) (TC y) = TC $ dzipWith (..*) x y
---          (./) x y = sipWith (../) x y
---          (.+) x y = sipWith (..+) x y
---          (.-) x y = sipWith (..-) x y
-
+          (./) (TC x) (TC y) = TC $ dzipWith (..*) x y
 
 class Sum t1 t2 t3 s1 s2 s3 c1 c2 c3 | s1 s2 -> s3, c1 c2 -> c3   where
   (.+) ::  TC t1 s1 c1 -> TC t2 s2 c2 -> TC t3 s3 c3
---  (.-) :: 
+  (.-) ::  TC t3 s1 c1 -> TC t2 s2 c2 -> TC t1 s3 c3
 
 instance  (DZipWith v1 v2 d1 d2 d3, DArith d1 d2 d3, SArith s1 s2 s3, TSum t1 t2 t3) =>  Sum t1 t2 t3 s1 s2 s3 (v1 d1) (v2 d2) (v2 d3) where
           (.+) (TC x) (TC y) = TC $ dzipWith (..+) x y
---          (.-) (TC x) (TC y) = TC $ dzipWith (..-) x y
+          (.-) (TC x) (TC y) = TC $ dzipWith (..-) x y
