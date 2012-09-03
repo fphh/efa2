e09a6f88ecec2c00c6f5043fa2672724b9d58fe7
Henning Thielemann <git@henning-thielemann.de>
2012-09-03 17:25:58 +0200

    Demo_SignalCalc: eliminate most warnings add to Cabal file

--- demo/Signal/Demo_SignalCalc.hs
+++ demo/Signal/Demo_SignalCalc.hs
@@ -1,75 +1,82 @@
-{-# LANGUAGE FlexibleContexts, 
-TypeOperators, 
-ExistentialQuantification, 
-RankNTypes #-}
-
-{- Skript to demonstrate Calculations and other Operations on Signals -} 
+{- | Skript to demonstrate Calculations and other Operations on Signals -}
 
 -- module Demo_Signal where
 
+import qualified EFA2.Display.Plot as Plot
 import qualified EFA2.Signal.Signal as S
-import EFA2.Display.DispSignal 
-import EFA2.Display.Plot
-import EFA2.Signal.Typ
-import EFA2.Signal.Base
-import EFA2.Signal.Data
-
+import EFA2.Display.DispSignal (sdisp)
 import EFA2.Signal.Signal
           (TC, Scalar, Signal, FSignal, FSamp, PFSamp, PSigL, UTFSig, Test1, Test2,
            PSig,TSig, Scal, FFSig,  FSig1,
            (.-), (.+), (./), (.*), (.++)) -- (&-), (&+), (&/), (&*), (&++))
-          
-import EFA2.Signal.Data (Data, Nil)
-import qualified Data.Vector as V
-import qualified Data.Vector.Unboxed as UV
+
+import EFA2.Signal.Typ (Typ, A, D, P, N, T, Tt)
+import EFA2.Signal.Data (UVec)
+import EFA2.Signal.Base (Val)
+
 
 -- Generate objects to work with
-offset = S.toScalar 0 :: Scal (Typ D P Tt) Val
+offset :: Scal (Typ D P Tt) Val
+offset = S.toScalar 0
 
 -- Time Vector
-time = S.fromList ([0,0.1..pi]++[pi])  :: TSig
+time :: TSig
+time = S.fromList ([0,0.1..pi]++[pi])
 
 -- time step midth vector
-mtime = S.avSig time :: TC FSignal (Typ A T Tt) (UVec Val) 
+mtime :: TC FSignal (Typ A T Tt) (UVec Val)
+mtime = S.avSig time
 
 -- time step vector
-dtime = S.deltaSig time :: TC FSignal (Typ D T Tt) (UVec Val) 
+dtime :: TC FSignal (Typ D T Tt) (UVec Val)
+dtime = S.deltaSig time
 
--- Constanter Wirkungsgrad
-n1 = S.toScalar 0.8 :: Scal (Typ A N Tt) Val
+-- constant efficiency
+n1 :: Scal (Typ A N Tt) Val
+n1 = S.toScalar 0.8
 
 -- Generate two Power Signals
-pSig1 = ((S.changeType (S.map sin time)).-offset).*((S.toScalar 1000):: Scal (Typ A N Tt) Val) ::  PSig
+pSig1, pSig2 :: PSig
+pSig1 =
+   ((S.changeType (S.map sin time)) .- offset)
+   .*
+   (S.toScalar 1000 :: Scal (Typ A N Tt) Val)
 pSig2 = pSig1.*n1
 
 -- Make Time-Step-Integration to get 1D energy flow signals
-fSig1 = S.sigPartInt time pSig1  :: FFSig
-fSig2 = S.sigPartInt time pSig2  :: FFSig
+fSig1, fSig2 :: FFSig
+fSig1 = S.sigPartInt time pSig1
+fSig2 = S.sigPartInt time pSig2
 
 {-
--- Calculate 1D average Power Signal 
-pavSig1 = fSig1./dtime :: FSig1 (Typ A P Tt) Val 
+-- Calculate 1D average Power Signal
+pavSig1 :: FSig1 (Typ A P Tt) Val
+pavSig1 = fSig1./dtime
 -}
 
 {-
 -- Calculate 1D efficiency signal
-nSig = fSig2./fSig1 :: FSig1 (Typ A N Tt)  Val
+nSig :: FSig1 (Typ A N Tt) Val
+nSig = fSig2./fSig1
 -}
 
 {-
--- calculate effektive efficiency value in two ways
-nVal1 = (S.sigFullInt time pSig2)./(S.sigFullInt time pSig1) ::  Scal (Typ A N Tt) Val
+-- calculate effective efficiency value in two ways
+nVal1 :: Scal (Typ A N Tt) Val
+nVal1 = S.sigFullInt time pSig2 ./ S.sigFullInt time pSig1
 -}
 
-nVal2 = (S.sigSum fSig2)./S.sigSum(fSig1) ::  Scal (Typ A N Tt) Val
+nVal2 :: Scal (Typ A N Tt) Val
+nVal2 = S.sigSum fSig2 ./ S.sigSum fSig1
 
-main = do  
-   
-  xyplot "Power" time pSig1  
-  
-{-  
+main :: IO ()
+main = do
+
+  Plot.xyplot "Power" time pSig1
+
+{-
 -- !! - doesn't compile, can't plot fsignal
-  xyplot "Energie" time eSig 
+  Plot.xyplot "Energie" time eSig
 -}
 
-  putStrLn(sdisp nVal2)  
+  putStrLn (sdisp nVal2)
