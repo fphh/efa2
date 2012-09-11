4933bac0f37214146c2c30b536c52d0498f81c68
Henning Thielemann <git@henning-thielemann.de>
2012-09-11 13:20:58 +0200

    make executable from Test modules

--- src/EFA2/Test/Test.hs
+++ src/EFA2/Test/Test.hs
@@ -1,16 +1,17 @@
+module Main where
 
-module EFA2.Test.Test where
+-- module EFA2.Test.Test where
 
 
 import qualified EFA2.Test.UtilsTest as UT
-import qualified EFA2.Test.SolverTest as ST
-import qualified EFA2.Test.IsVarTest as IVT
+-- import qualified EFA2.Test.SolverTest as ST
+-- import qualified EFA2.Test.IsVarTest as IVT
 
+import Control.Functor.HT (void)
 
 
 main :: IO ()
 main = do
-  --UT.runTests
-  --ST.runTests
-  IVT.runTests
-  return ()
+   void $ UT.runTests
+   -- void $ ST.runTests
+   -- void $ IVT.runTests
--- src/EFA2/Test/UtilsTest.hs
+++ src/EFA2/Test/UtilsTest.hs
@@ -5,17 +5,20 @@ module EFA2.Test.UtilsTest where
 import qualified Data.Set as S
 import qualified Data.List as L
 
-import Test.QuickCheck
-import Test.QuickCheck.All
+import EFA2.Utils.Utils (pairs, yazf, unique)
 
+import Test.QuickCheck (Property, (==>))
+import Test.QuickCheck.All (quickCheckAll)
 
-import EFA2.Utils.Utils
 
 -- The difference between 'unique' and (toList . fromList) is
 -- that 'unique' does not sort. Therefore, 'unique' has linear
 -- running time, whereas (toList . fromList) has n*log n running time.
+prop_unique :: Ord a => [a] -> Bool
 prop_unique xs = L.sort (unique xs) == S.toList (S.fromList xs)
 
-prop_dmap xs = length xs > 0 ==> yazf (,) xs xs == dmap (,) xs
+prop_dmap :: Eq a => [a] -> Property
+prop_dmap xs = length xs > 0 ==> yazf (,) xs xs == pairs xs
 
+runTests :: IO Bool
 runTests = $quickCheckAll
