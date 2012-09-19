ccb995ecbc8ae687fb2f42d6cbe77bf61675996d
Henning Thielemann <git@henning-thielemann.de>
2012-09-19 23:17:16 +0200

    Report: resolve some name clashes and remove some unnecessary variables

--- src/EFA2/Display/Report.hs
+++ src/EFA2/Display/Report.hs
@@ -63,43 +63,43 @@ tvcat = foldl1 tvapp
 
 
 tvapp :: Table -> Table -> Table
-tvapp x1 x2 = if check then Table {tableTitle = tableTitle x1 ++ " ++  " ++ tableTitle x2,
-                                   tableFormat = f (tableFormat x1) (tableFormat x2),
-                                   tableData = g (tableData x1) (tableData x2),              
-                                   tableSubTitle = tableSubTitle x1 ++ " ++  " ++ tableSubTitle x2} else error m 
-                                   where 
+tvapp y1 y2 = if check then Table {tableTitle = tableTitle y1 ++ " ++  " ++ tableTitle y2,
+                                   tableFormat = f (tableFormat y1) (tableFormat y2),
+                                   tableData = g (tableData y1) (tableData y2),
+                                   tableSubTitle = tableSubTitle y1 ++ " ++  " ++ tableSubTitle y2} else error m
+                                   where
                                          g :: TableData -> TableData -> TableData
-                                         g x1 x2 = TableData {titleRow = titleRow x1, 
+                                         g x1 x2 = TableData {titleRow = titleRow x1,
                                                              tableBody = tableBody x1 ++ tableBody x2,
                                                              titleCols = titleCols x1 ++ titleCols x2,
-                                                             endCols = endCols x1 ++ endCols x2}           
-    
+                                                             endCols = endCols x1 ++ endCols x2}
+
                                          f :: TableFormat -> TableFormat -> TableFormat
                                          f x1 x2 = TableFormat {colFormat = maxColWidth (colFormat x1) (colFormat x2),
                                                                rowFormat = rowFormat x1 ++ (tail $ rowFormat x2)}
 
-                                         check = (titleRow $ tableData x1) ==  (titleRow $ tableData x2)
+                                         check = (titleRow $ tableData y1) ==  (titleRow $ tableData y2)
                                          m = "Error in tvCat -- not same column labels"
-                
+
 thcat :: [Table] -> Table
 thcat = foldl1 thapp
 
 thapp :: Table -> Table -> Table
-thapp x1 x2 = if check then Table {tableTitle = tableTitle x1 ++ " ++  " ++ tableTitle x2,
-                                   tableFormat = f (tableFormat x1) (tableFormat x2),
-                                   tableData = g (tableData x1) (tableData x2),              
-                                   tableSubTitle = tableSubTitle x1 ++ " ++  " ++ tableSubTitle x2} else error m             
+thapp y1 y2 = if check then Table {tableTitle = tableTitle y1 ++ " ++  " ++ tableTitle y2,
+                                   tableFormat = f (tableFormat y1) (tableFormat y2),
+                                   tableData = g (tableData y1) (tableData y2),
+                                   tableSubTitle = tableSubTitle y1 ++ " ++  " ++ tableSubTitle y2} else error m
               where g :: TableData -> TableData -> TableData
-                    g x1 x2 = TableData {titleRow = titleRow x1++titleRow x2, 
+                    g x1 x2 = TableData {titleRow = titleRow x1++titleRow x2,
                                         tableBody = L.zipWith (++) (tableBody x1) (tableBody x2),
                                         titleCols = titleCols x1,
-                                        endCols = endCols x1}           
-    
+                                        endCols = endCols x1}
+
                     f :: TableFormat -> TableFormat -> TableFormat
                     f x1 x2 = TableFormat {colFormat = (init $ colFormat x1)++(init $ tail $ colFormat x2)++[(last $ colFormat x2)],
                                            rowFormat = rowFormat x1}
-                    check = (titleCols $ tableData x1) == (titleCols $ tableData x2) && (endCols $ tableData x1) == (endCols $ tableData x2)  
-                    m = "Error in thcat - not same column title and end row"                           
+                    check = (titleCols $ tableData y1) == (titleCols $ tableData y2) && (endCols $ tableData y1) == (endCols $ tableData y2)
+                    m = "Error in thcat - not same column title and end row"
 
 
 maxColWidth :: ColFormat -> ColFormat -> ColFormat
@@ -121,10 +121,9 @@ makeTable  os t = PP.text (tableTitle t) PP.$$ (makeCol os rf $ map (makeRow os 
     rf = rowFormat $ tableFormat t
     cf = colFormat $ tableFormat t
     cft = if transpose then maxColWidth cf (repeat (getMaxColWidth cf,HLeft)) else cf
-    td = tableData t
     x = buildDocTable $ tableData t
-    transpose = L.elem (RVertical) os
-    xt = if transpose then L.transpose x else x 
+    transpose = L.elem RVertical os
+    xt = if transpose then L.transpose x else x
 
 -- | Generate doc table including title rows and colums
 buildDocTable :: TableData -> [[Cell]]
@@ -152,8 +151,8 @@ class ToTable a where
       toTable :: ROpts -> (String,a) -> [Table]
 
 instance (Show a) => ToTable [[a]] where
-      toTable os (ti,xs)  = [Table {tableTitle = "Matrix - " ++ ti,
-                                    tableFormat = tf,
+      toTable _os (ti,xs)  = [Table {tableTitle = "Matrix - " ++ ti,
+                                    tableFormat = autoFormat td,
                                     tableData = td,
                                     tableSubTitle = ""}]
 
@@ -161,7 +160,6 @@ instance (Show a) => ToTable [[a]] where
                               titleCols = [],
                               titleRow = [],
                               endCols = []}
-              tf = autoFormat td
 
 -- | convert raw data to doc elements, using given function
 toDoc :: (a->String) -> a -> Cell
