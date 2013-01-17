276932fcb7c7ac80136b798715180cffb7c93cb1
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2013-01-17 12:59:35 +0100

    beispiel für csv-import

--- src/EFA/IO/Import.hs
+++ src/EFA/IO/Import.hs
@@ -1,17 +1,4 @@
-module EFA.IO.Import (module EFA.IO.Import) where
-
-import qualified Data.Map as M 
-
-import Data.Ratio (Ratio, approxRational)
-
-import EFA.Signal.SequenceData (Record(Record), SigId(SigId))
-
---import Text.ParserCombinators.Parsec
-import Data.List.HT (chop)
-
-import qualified EFA.Signal.Signal as S
-import qualified EFA.Signal.Vector as SV
-
+module EFA.IO.Import (modelicaCSVImport) where
 
 -- Modelica CSV Import -----------------------------------------------------------------  
 
@@ -24,23 +11,102 @@ import qualified EFA.Signal.Vector as SV
 
 -}
 
+
+import qualified Data.Map as M 
+
+import EFA.Signal.SequenceData (Record(Record), SigId(SigId))
+
+import qualified EFA.Signal.Signal as S
+import qualified EFA.Signal.Vector as SV
+
+import Text.ParserCombinators.Parsec
+
+
+csvFile :: Parser [[String]]
+csvFile = endBy line eol
+
+line :: Parser [String]
+line = sepBy cell (char ',')
+
+cell :: Parser String
+cell = quotedCell <|> many (noneOf ",\n\r")
+
+quotedCell :: Parser String
+quotedCell = 
+    do _ <- char '"'
+       content <- many quotedChar
+       _ <- char '"' <?> "quote at end of cell"
+       return content
+
+quotedChar :: Parser Char
+quotedChar =
+        noneOf "\""
+    <|> try (string "\"\"" >> return '"')
+
+eol :: Parser String
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> "end of line"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
+modelicaCSVParse :: String -> Either ParseError [[String]] -> Record
+modelicaCSVParse _ (Right strs@(("time":_):_)) = makeRecord strs
+modelicaCSVParse path (Right []) = error ("Empty csv file: " ++ path)
+modelicaCSVParse path (Right _) =
+  error ("First column of " ++ path ++ " is not \"time\"")
+modelicaCSVParse path (Left err) =
+  error ("Parse error in file " ++ show path ++ ": " ++ show err)
+
+
+makeRecord :: [[String]] -> Record
+makeRecord [] = error "This is not possible!"
+makeRecord (h:hs) =
+  Record (S.fromList time) (M.fromList $ zip sigIdents (map S.fromList sigs))
+  where sigIdents = map SigId (tail h)
+        time:sigs = SV.transpose (map (map read . init) hs)
+
 -- | Main Modelica CSV Import Function
-modelicaCSVImport:: FilePath -> IO Record
+modelicaCSVImport :: FilePath -> IO Record
 modelicaCSVImport path = do 
   text <- readFile path
-  return $ modelicaCSVParse text
+  return $ modelicaCSVParse path (parseCSV text)
+
+{-
+modelicaCSVImport' :: FilePath -> IO Record
+modelicaCSVImport' path = do 
+  text <- readFile path
+  return $ modelicaCSVParse' text
 
 -- | Parse modelica-generated CSV - files with signal logs   
-modelicaCSVParse :: String -> Record
-modelicaCSVParse text = rec
-  where csvlines = lines text -- read get all lines
-        header =  csvParseHeaderLine $ head csvlines  -- header with labels in first line       
-        sigIdents = map SigId (tail header) -- first column is "time" / use Rest
-        columns = SV.transpose (map csvParseDataLine $ tail csvlines) -- rest of lines contains data / transpose from columns to lines
-        time = if (head header) == "time" then head columns else error $ "Error in csvImport - first column not time : " ++ (head header)
-        sigs = tail columns -- generate signals from rest of columns
-        rec = Record (S.fromList time)  (M.fromList $ zip sigIdents (map S.fromList sigs)) -- generate Record with signal Map
-        
+modelicaCSVParse' :: String -> Record
+modelicaCSVParse' text = rec
+  where 
+        -- read get all lines
+        csvlines = lines text
+
+        -- header with labels in first line
+        header =  csvParseHeaderLine $ head csvlines
+
+        -- first column is "time" / use Rest
+        sigIdents = map SigId (tail header)
+
+        -- rest of lines contains data / transpose from columns to lines
+        columns = SV.transpose (map csvParseDataLine $ tail csvlines)
+        time = if head header == "time"
+                  then head columns
+                  else error $ "Error in csvImport - first column not time : " ++ (head header)
+
+        -- generate signals from rest of columns
+        sigs = tail columns
+
+        -- generate Record with signal Map
+        rec = Record (S.fromList time)
+                     (M.fromList $ zip sigIdents (map S.fromList sigs))
+ 
 -- | Parse CSV Header Line
 csvParseHeaderLine :: String -> [String]  
 csvParseHeaderLine line = init $ map read (chop (','==) line)   -- (init . tail) to get rid of Modelica " " quotes 
@@ -55,3 +121,4 @@ instance ParseCVS Double where
 
 instance Integral int => ParseCVS (Ratio int) where
          csvParseDataLine line = init $ map (realToFrac . flip approxRational (0.001::Rational) . read) (chop (','==) line)
+-}
