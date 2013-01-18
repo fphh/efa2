c633e4663fbdfba3e6f9f9dd9d4748f6fb083b77
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2013-01-18 11:25:15 +0100

    beispiel zeigt jetzt topologie an

--- examples/vehicle/src/Main.hs
+++ examples/vehicle/src/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Data.Foldable (foldMap)
+import qualified EFA.Graph.Topology.StateAnalysis as StateAnalysis
 
 import qualified Data.Map as M
 
@@ -23,22 +24,22 @@ import qualified EFA.Signal.Plot as PL
 import EFA.Signal.Sequence (makeSequence, makeSeqFlowGraph)
 import qualified EFA.Signal.Signal as Sig
 
-import EFA.Graph.Draw (drawTopology)
+import EFA.Graph.Draw -- (drawTopology)
 
 
 sec0, sec1, sec2, sec3, sec4 :: Idx.Section
 sec0 :~ sec1 :~ sec2 :~ sec3 :~ sec4 :~ _ = Stream.enumFrom $ Idx.Section 0
 
-node0, node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11 :: Idx.Node
-node0 :~ node1 :~ node2 :~ node3 :~ node4 :~ node5 :~ node6 :~ node7 :~ node8 :~ node9 :~ node10 :~ node11 :~ _ = Stream.enumFrom $ Idx.Node 0
+tank, con_engine, el_cross, battery, node4, node5, node6, node7, node8, node9, node10, node11 :: Idx.Node
+tank :~ con_engine :~ el_cross :~ battery :~ node4 :~ node5 :~ node6 :~ node7 :~ node8 :~ node9 :~ node10 :~ node11 :~ _ = Stream.enumFrom $ Idx.Node 0
 
 
 topo :: TD.Topology
 topo = Gr.mkGraph ns (makeEdges es)
-  where ns = [(node0, TD.Source),    -- tank
-              (node1 , TD.Crossing), -- connection engine / generator
-              (node2, TD.Crossing),  -- electric crossing
-              (node3, TD.Storage),   -- battery   
+  where ns = [(tank, TD.Source),
+              (con_engine , TD.Crossing),
+              (el_cross, TD.Crossing),
+              (battery, TD.Storage),
               (node4, TD.Crossing),  -- electric crossing to vehicle electric system
               (node5, TD.Crossing),  -- connection motor / gearbox
               (node6, TD.Crossing),  -- connection brakes
@@ -48,10 +49,10 @@ topo = Gr.mkGraph ns (makeEdges es)
               (node10, TD.Sink),     -- vehicle brakes
               (node11, TD.Storage)]  -- vehicle inertia
              
-        es = [(node0, node1),  -- ic engine
-              (node1, node2),  -- generator
-              (node2, node3),  -- battery
-              (node2, node4),  -- electric connection        
+        es = [(tank, con_engine),  -- ic engine
+              (con_engine, el_cross),  -- generator
+              (el_cross, battery),  -- battery
+              (el_cross, node4),  -- electric connection        
               (node4, node5),  -- motor
               (node5, node6),  -- gearbox 
               (node6, node7),  -- wheels             
@@ -78,12 +79,12 @@ main = do
                              (M.map (Sig.fromList . Sig.toList) pMap)
 
       pMap =  M.fromList [
-         (SD.PPosIdx node0 node1, sigMap M.! (SD.SigId "s1")),
-         (SD.PPosIdx node1 node0, sigMap M.! (SD.SigId "s2")),
-         (SD.PPosIdx node1 node2, sigMap M.! (SD.SigId "s3")),
-         (SD.PPosIdx node2 node1, sigMap M.! (SD.SigId "s4")),
-         (SD.PPosIdx node1 node3, sigMap M.! (SD.SigId "s5")),
-         (SD.PPosIdx node3 node1, sigMap M.! (SD.SigId "s6")) ]
+         (SD.PPosIdx tank con_engine, sigMap M.! (SD.SigId "s1")),
+         (SD.PPosIdx con_engine tank, sigMap M.! (SD.SigId "s2")),
+         (SD.PPosIdx con_engine el_cross, sigMap M.! (SD.SigId "s3")),
+         (SD.PPosIdx el_cross con_engine, sigMap M.! (SD.SigId "s4")),
+         (SD.PPosIdx con_engine battery, sigMap M.! (SD.SigId "s5")),
+         (SD.PPosIdx battery con_engine, sigMap M.! (SD.SigId "s6")) ]
 -- seq = chopAtZeroCrossingsPowerRecord pRec
 
        -- sequ = makeSequence pRec
@@ -92,7 +93,10 @@ main = do
 
 --      env = EqGen.solve given sequTopo
 
-  print pRec
-  -- drawTopology topo    
+  -- print pRec
+      sol = StateAnalysis.advanced topo
+
+  putStrLn ("Number of flow states: " ++ show (length sol))
+  drawTopologyXs' [head sol]
   -- drawTopology sequTopo env
-  PL.rPlot ("Test",pRec) 
+  -- PL.rPlot ("Test",pRec) 
