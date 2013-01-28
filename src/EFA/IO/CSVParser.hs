8975c5024ee3a7294522db9279aa92696eaa1d9a
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2013-01-28 10:58:21 +0100

    ein paar resets

--- src/EFA/IO/CSVParser.hs
+++ src/EFA/IO/CSVParser.hs
@@ -8,9 +8,6 @@ module EFA.IO.CSVParser (csvFile) where
 import Text.ParserCombinators.Parsec
 
 
-import EFA.Utility ((>>!))
-
-
 -- | Takes a separator character.
 csvFile :: Char -> Parser [[String]]
 csvFile sepChar = endBy (line sepChar) eol
@@ -28,7 +25,6 @@ quotedCell =
   between (char '"') (char '"' <?> "quote at end of cell") $
   many quotedChar
 
--- Warum funktioniert hier (>>!) nicht?
 quotedChar :: Parser Char
 quotedChar =
         noneOf "\""
