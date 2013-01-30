1f9634c6cca63ef606ea668db9c7af31bd99ee89
Philipp Guttenberg <guttenberg@energiefluss.info>
2013-01-30 20:11:55 +0100

    Aktuelle Versionen von CSV- und ASCII-Import wieder hergestellt und neue RecordData-Typen eingepflegt

--- src/EFA/IO/ASCIIImport.hs
+++ src/EFA/IO/ASCIIImport.hs
@@ -1,47 +1,33 @@
 
+-- | ASCII Import
 
 module EFA.IO.ASCIIImport (modelicaASCIIImport) where
 
--- | ASCII Import
-
 import qualified Data.Map as M
-import Text.ParserCombinators.Parsec (parse, ParseError)
+import Text.ParserCombinators.Parsec (parse)
+import EFA.Signal.Base (Val)
 
 import EFA.Signal.Record (SignalRecord(SignalRecord), SigId(SigId))
 
 import qualified EFA.Signal.Signal as S
 import qualified EFA.Signal.Vector as SV
 
-import qualified EFA.Signal.Base as SB
-
-
 import EFA.IO.CSVParser (csvFile)
 
 
 
-
-modelicaASCIIParse :: String -> Either ParseError [[String]] -> SignalRecord [] SB.Val
-modelicaASCIIParse _ (Right strs) = makeASCIIRecord strs
-modelicaASCIIParse path (Left err) =
-  error ("Parse error in file " ++ show path ++ ": " ++ show err)
-
-
-makeASCIIRecord :: [[String]] -> SignalRecord [] SB.Val
+makeASCIIRecord :: [[String]] -> SignalRecord [] Val
 makeASCIIRecord [] = error "This is not possible!"
 makeASCIIRecord hs =
   SignalRecord (S.fromList time) (M.fromList $ zip sigIdents (map S.fromList sigs))
-  where sigIdents = map (SigId . ("sig_" ++) . addLeadingZeros . show) [0..] 
+  where sigIdents = map (SigId . ("sig_" ++) . show) [(0::Int)..]
         time:sigs = SV.transpose (map (map read . init) hs)
-        -- Always start getDecade counter with 0
-        addLeadingZeros x = replicate ((getDecade(toRational (length sigs)) 0)-length x) '0' ++ x                
-        getDecade x decCounter = if x > 1 then (getDecade (x/10) (decCounter+1)) else decCounter
-          
 
-parseASCII :: String -> Either ParseError [[String]]
-parseASCII input = parse (csvFile ' ') "(unknown)" input
-
--- | Main ASCIIII Import Function
-modelicaASCIIImport :: FilePath -> IO (SignalRecord [] SB.Val)
-modelicaASCIIImport path = do 
+-- | Main ASCII Import Function
+modelicaASCIIImport ::  FilePath -> IO (SignalRecord [] Val)
+modelicaASCIIImport path = do
   text <- readFile path
-  return $ modelicaASCIIParse path (parseASCII text)
+  case parse (csvFile ' ') path text of
+    Left err ->
+      ioError $ userError $ "Parse error in file " ++ show err
+    Right table -> return $ makeASCIIRecord table
