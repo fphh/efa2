bdd5ab00e1df6f9d81d7a3afeccd4e6fdcef60e1
Henning Thielemann <git@henning-thielemann.de>
2013-03-03 01:20:48 +0100

    demo/lookupEta: added to Cabal file
    updated to current library

--- demo/lookupEta/Main.hs
+++ demo/lookupEta/Main.hs
@@ -4,46 +4,46 @@
 module Main where
 
 import qualified EFA.Equation.Env as Env
-import qualified EFA.Equation.System as EqSys
-import EFA.Equation.System ((=.=))
+import qualified EFA.Equation.Absolute as EqSys
+import EFA.Equation.Absolute ((.=), (=.=))
+import EFA.Example.Utility (constructSeqTopo, edgeVar, makeEdges)
 
 import qualified EFA.Graph.Topology.Index as Idx
+import qualified EFA.Graph.Topology.Node as Node
 import qualified EFA.Graph.Topology as TD
 import qualified EFA.Utility.Stream as Stream
 import EFA.Utility.Stream (Stream((:~)))
 import EFA.Utility (checkedLookup)
 import EFA.Graph (mkGraph)
-import EFA.Example.Utility ((.=), constructSeqTopo, edgeVar, makeEdges, recAbs)
 
 import qualified EFA.Report.Format as Format
 import EFA.Report.FormatValue (formatValue)
 
-import Data.Monoid ((<>))
-import Data.Foldable (foldMap)
+import Data.Monoid (mconcat, (<>))
 
 
 sec0 :: Idx.Section
 sec0 :~ _ = Stream.enumFrom $ Idx.Section 0
 
-sink, source :: Idx.Node
-sink :~ (source :~ _) = Stream.enumFrom $ Idx.Node 0
+sink, source :: Node.Int
+sink :~ (source :~ _) = Stream.enumFrom $ Node.Int 0
 
-linearOne :: TD.Topology
+linearOne :: TD.Topology Node.Int
 linearOne = mkGraph nodes (makeEdges edges)
   where nodes = [(sink, TD.AlwaysSink), (source, TD.AlwaysSource)]
         edges = [(source, sink)]
 
-seqTopo :: TD.SequFlowGraph
+seqTopo :: TD.SequFlowGraph Node.Int
 seqTopo = constructSeqTopo linearOne [0]
 
 enRange :: [Double]
 enRange = 0.01:[0.5, 1 .. 9]
 
-c :: EqSys.ExprWithVars s a
-c = edgeVar EqSys.power sec0 source sink
+c :: Idx.Power Node.Int
+c = edgeVar Idx.Power sec0 source sink
 
-n :: EqSys.ExprWithVars s a
-n = edgeVar EqSys.eta sec0 source sink
+eta :: Idx.Eta Node.Int
+eta = edgeVar Idx.Eta sec0 source sink
 
 eval :: [(Double, Double)] -> Double -> Double
 eval lt pin =
@@ -52,25 +52,31 @@ eval lt pin =
        (_, v):_ -> v
 
 
-lookupEta :: EqSys.ExprWithVars s Double -> EqSys.ExprWithVars s Double
-lookupEta = EqSys.makeFunc $ eval table
+lookupEta ::
+   EqSys.Expression Node.Int s a v Double ->
+   EqSys.Expression Node.Int s a v Double
+lookupEta = EqSys.liftF $ eval table
   where table = zip [0..9] [0, 0.1, 0.3, 0.6, 0.7, 0.65, 0.6, 0.4, 0.35, 0.1]
 
-given :: Double -> EqSys.EquationSystem s Double
+given :: Double -> EqSys.EquationSystem Node.Int s Double Double
 given p =
-   foldMap (uncurry (.=)) $
-   (EqSys.dtime sec0, 1) :
-   (edgeVar EqSys.power sec0 source sink, p) : []
+   mconcat $
 
-eta :: Idx.Eta
-eta = edgeVar (Idx.Eta recAbs) sec0 source sink
+   (Idx.DTime sec0 .= 1) :
+   (c .= p) :
+   []
 
 
 solve :: Double -> String
 solve p =
-  let env = EqSys.solve ((n =.= lookupEta c) <> given p) seqTopo
+  let env =
+         EqSys.solve
+            ((EqSys.getSignalVar eta =.= lookupEta (EqSys.getSignalVar c))
+               <> given p)
+            seqTopo
   in  show p ++ " " ++
-      Format.unUnicode (formatValue (checkedLookup (Env.etaMap env) eta))
+      Format.unUnicode (formatValue (Env.unAbsolute
+         (checkedLookup (Env.etaMap $ Env.signal env) eta)))
 
 main :: IO ()
 main =
