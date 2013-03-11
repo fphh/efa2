23d42cdd13ede686a798476923ffc467e99e47b1
Henning Thielemann <git@henning-thielemann.de>
2013-03-11 18:30:09 +0100

    Plot.stack: taken from demo/additiveTerms

--- demo/additiveTerms/Main.hs
+++ demo/additiveTerms/Main.hs
@@ -26,28 +26,15 @@ import qualified EFA.Graph.Topology as TD
 import qualified EFA.Graph.Draw as Draw
 import qualified EFA.Graph as Gr
 
+import qualified EFA.Signal.Plot as Plot
+
 import qualified EFA.Report.Format as Format
 import EFA.Report.FormatValue (FormatValue, formatValue)
-
-
-import qualified Graphics.Gnuplot.Advanced as GP
-
-import qualified Graphics.Gnuplot.Frame as Frame
-import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
-import qualified Graphics.Gnuplot.Frame.OptionSet.Style as OptsStyle
-import qualified Graphics.Gnuplot.Frame.OptionSet.Histogram as Histogram
-
-import qualified Graphics.Gnuplot.Plot.TwoDimensional as Plot2D
-import qualified Graphics.Gnuplot.Graph.TwoDimensional as Graph2D
-
-import qualified Graphics.Gnuplot.LineSpecification as LineSpec
 
 
 import qualified Data.Foldable as Fold
 import qualified Data.Map as Map
 import qualified Data.NonEmpty as NonEmpty
-import Control.Functor.HT (void)
-import Data.Foldable (foldMap, )
 import Data.Monoid (mempty, (<>))
 import Data.Tuple.HT (mapFst)
 
@@ -161,23 +148,6 @@ givenNumeric =
 eout :: Idx.Energy Node.Int
 eout = edgeVar Idx.Energy sec0 node2 node1
 
-histogram ::
-   (Fold.Foldable f, FormatValue term) =>
-   f (term, Double) -> Frame.T (Graph2D.T Int Double)
-histogram =
-   Frame.cons (
-      Opts.title "Decomposition of total output energy" $
-      Histogram.rowstacked $
-      OptsStyle.fillBorderLineType (-1) $
-      OptsStyle.fillSolid $
-      Opts.xTicks2d [(Format.unASCII $ formatValue $
-                      Idx.delta $ Var.index eout, 0)] $
-      Opts.xRange2d (-1,3) $
-      Opts.deflt) .
-   foldMap (\(term,val) ->
-      fmap (Graph2D.lineSpec
-              (LineSpec.title (Format.unASCII $ formatValue term) LineSpec.deflt)) $
-      Plot2D.list Graph2D.histograms [val])
 
 
 mainNumeric :: IO ()
@@ -199,7 +169,8 @@ mainNumeric = do
                Fold.forM_ assigns $ \(term,val) -> do
                   putStrLn $
                      (Format.unUnicode $ formatValue term) ++ " = " ++ show val
-               void $ GP.plotDefault $ histogram assigns
+               Plot.stackIO "Decomposition of total output energy"
+                  (Idx.delta $ Var.index eout) assigns
 
 
 main :: IO ()
