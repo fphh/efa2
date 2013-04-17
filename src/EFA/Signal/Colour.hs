b0a042f652ae9975937a2879ff0b93b9f0d66f79
Henning Thielemann <git@henning-thielemann.de>
2013-04-17 19:35:00 +0200

    Signal.Colour: strict imports

--- src/EFA/Signal/Colour.hs
+++ src/EFA/Signal/Colour.hs
@@ -1,10 +1,9 @@
 
-
 module EFA.Signal.Colour where
 
-import Data.Colour
-import Data.Colour.Names as Colour
+import qualified Data.Colour.Names as Colour
 import Data.Colour.SRGB (sRGB24show)
+import Data.Colour (Colour)
 
 import qualified Data.Stream as Stream
 import Data.Stream (Stream)
@@ -21,151 +20,152 @@ adorn :: Traversable f => f a -> f (C, a)
 adorn = zipWithTraversable (,) colours
 
 defltColour :: C
-defltColour = showf red
+defltColour = showf Colour.red
 
 colours :: Stream C
 colours = Stream.cycle cls
 
 cls :: [C]
-cls = map showf [
-  red,
-  blue,
-  yellow,
-  green,
-  orange,
-  gray,
-  magenta,
-  cyan,
-  chocolate,
-  olive,
-  purple,
-  white,
-  violet,
-  lightblue,
-  darkblue,
-  lightcoral,
-  darkcyan,
-  lightcyan,
-  darkgoldenrod,
-  lightgoldenrodyellow,
-  darkgray,
-  lightgray,
-  darkgreen,
-  lightgreen,
-  darkkhaki,
-  lightpink,
-  darkmagenta,
-  lightsalmon,
-  darkolivegreen,
-  lightseagreen,
-  darkorange,
-  lightskyblue,
-  darkorchid,
-  lightslategray,
-  darkred,
-  lightslategrey,
-  darksalmon,
-  lightsteelblue,
-  darkseagreen,
-  lightyellow,
-  darkslateblue,
-  darkslategray,
-  darkturquoise,
-  darkviolet,
-  deeppink,
-  deepskyblue,
-  dimgray,
-  dodgerblue,
-  firebrick,
-  floralwhite,
-  forestgreen,
-  fuchsia,
-  gainsboro,
-  ghostwhite,
-  gold,
-  goldenrod,
-  greenyellow,
-  honeydew,
-  hotpink,
-  indianred,
-  indigo,
-  ivory,
-  khaki,
-  lavender,
-  lavenderblush,
-  lawngreen,
-  lemonchiffon,
-  lime,
-  limegreen,
-  linen,
-  maroon,
-  mediumaquamarine,
-  mediumblue,
-  mediumorchid,
-  mediumpurple,
-  mediumseagreen,
-  mediumslateblue,
-  mediumspringgreen,
-  mediumturquoise,
-  mediumvioletred,
-  midnightblue,
-  mintcream,
-  mistyrose,
-  moccasin,
-  navajowhite,
-  navy,
-  oldlace,
-  olivedrab,
-  orangered,
-  orchid,
-  palegoldenrod,
-  palegreen,
-  paleturquoise,
-  palevioletred,
-  aliceblue,
-  antiquewhite,
-  aqua,
-  aquamarine,
-  azure,
-  beige,
-  bisque,
-  black,
-  blanchedalmond,
-  blueviolet,
-  brown,
-  burlywood,
-  cadetblue,
-  chartreuse,
-  coral,
-  cornflowerblue,
-  cornsilk,
-  crimson,
-  papayawhip,
-  peachpuff,
-  peru,
-  pink,
-  plum,
-  powderblue,
-  rosybrown,
-  royalblue,
-  saddlebrown,
-  salmon,
-  sandybrown,
-  seagreen,
-  seashell,
-  sienna,
-  silver,
-  skyblue,
-  slateblue,
-  slategray,
-  snow,
-  springgreen,
-  steelblue,
-  Colour.tan,
-  teal,
-  thistle,
-  tomato,
-  turquoise,
-  wheat,
-  whitesmoke,
-  yellowgreen ]
+cls = map showf $
+  Colour.red :
+  Colour.blue :
+  Colour.yellow :
+  Colour.green :
+  Colour.orange :
+  Colour.gray :
+  Colour.magenta :
+  Colour.cyan :
+  Colour.chocolate :
+  Colour.olive :
+  Colour.purple :
+  Colour.white :
+  Colour.violet :
+  Colour.lightblue :
+  Colour.darkblue :
+  Colour.lightcoral :
+  Colour.darkcyan :
+  Colour.lightcyan :
+  Colour.darkgoldenrod :
+  Colour.lightgoldenrodyellow :
+  Colour.darkgray :
+  Colour.lightgray :
+  Colour.darkgreen :
+  Colour.lightgreen :
+  Colour.darkkhaki :
+  Colour.lightpink :
+  Colour.darkmagenta :
+  Colour.lightsalmon :
+  Colour.darkolivegreen :
+  Colour.lightseagreen :
+  Colour.darkorange :
+  Colour.lightskyblue :
+  Colour.darkorchid :
+  Colour.lightslategray :
+  Colour.darkred :
+  Colour.lightslategrey :
+  Colour.darksalmon :
+  Colour.lightsteelblue :
+  Colour.darkseagreen :
+  Colour.lightyellow :
+  Colour.darkslateblue :
+  Colour.darkslategray :
+  Colour.darkturquoise :
+  Colour.darkviolet :
+  Colour.deeppink :
+  Colour.deepskyblue :
+  Colour.dimgray :
+  Colour.dodgerblue :
+  Colour.firebrick :
+  Colour.floralwhite :
+  Colour.forestgreen :
+  Colour.fuchsia :
+  Colour.gainsboro :
+  Colour.ghostwhite :
+  Colour.gold :
+  Colour.goldenrod :
+  Colour.greenyellow :
+  Colour.honeydew :
+  Colour.hotpink :
+  Colour.indianred :
+  Colour.indigo :
+  Colour.ivory :
+  Colour.khaki :
+  Colour.lavender :
+  Colour.lavenderblush :
+  Colour.lawngreen :
+  Colour.lemonchiffon :
+  Colour.lime :
+  Colour.limegreen :
+  Colour.linen :
+  Colour.maroon :
+  Colour.mediumaquamarine :
+  Colour.mediumblue :
+  Colour.mediumorchid :
+  Colour.mediumpurple :
+  Colour.mediumseagreen :
+  Colour.mediumslateblue :
+  Colour.mediumspringgreen :
+  Colour.mediumturquoise :
+  Colour.mediumvioletred :
+  Colour.midnightblue :
+  Colour.mintcream :
+  Colour.mistyrose :
+  Colour.moccasin :
+  Colour.navajowhite :
+  Colour.navy :
+  Colour.oldlace :
+  Colour.olivedrab :
+  Colour.orangered :
+  Colour.orchid :
+  Colour.palegoldenrod :
+  Colour.palegreen :
+  Colour.paleturquoise :
+  Colour.palevioletred :
+  Colour.aliceblue :
+  Colour.antiquewhite :
+  Colour.aqua :
+  Colour.aquamarine :
+  Colour.azure :
+  Colour.beige :
+  Colour.bisque :
+  Colour.black :
+  Colour.blanchedalmond :
+  Colour.blueviolet :
+  Colour.brown :
+  Colour.burlywood :
+  Colour.cadetblue :
+  Colour.chartreuse :
+  Colour.coral :
+  Colour.cornflowerblue :
+  Colour.cornsilk :
+  Colour.crimson :
+  Colour.papayawhip :
+  Colour.peachpuff :
+  Colour.peru :
+  Colour.pink :
+  Colour.plum :
+  Colour.powderblue :
+  Colour.rosybrown :
+  Colour.royalblue :
+  Colour.saddlebrown :
+  Colour.salmon :
+  Colour.sandybrown :
+  Colour.seagreen :
+  Colour.seashell :
+  Colour.sienna :
+  Colour.silver :
+  Colour.skyblue :
+  Colour.slateblue :
+  Colour.slategray :
+  Colour.snow :
+  Colour.springgreen :
+  Colour.steelblue :
+  Colour.tan :
+  Colour.teal :
+  Colour.thistle :
+  Colour.tomato :
+  Colour.turquoise :
+  Colour.wheat :
+  Colour.whitesmoke :
+  Colour.yellowgreen :
+  []
