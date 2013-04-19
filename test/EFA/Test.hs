42cbbcafd72e205a7f16d8b70cd5cbbe39edbbd0
Henning Thielemann <git@henning-thielemann.de>
2013-04-19 10:37:07 +0200

    Test.StateAnalysis: tests taken from Topology.StateAnalysis

--- test/EFA/Test.hs
+++ test/EFA/Test.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import qualified EFA.Test.StateAnalysis as StateAnalysis
 import qualified EFA.Test.MultiValue as MultiValue
 import qualified EFA.Test.Stack as Stack
 import qualified EFA.Test.Utility as Util
@@ -10,6 +11,7 @@ import Control.Functor.HT (void)
 
 main :: IO ()
 main = do
+   void $ StateAnalysis.runTests
    void $ MultiValue.runTests
    void $ Stack.runTests
    void $ Util.runTests
