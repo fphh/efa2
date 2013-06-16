afd5c3e4657370d4ffa73da7e2d7c47f29c9529d
Henning Thielemann <git@henning-thielemann.de>
2013-06-16 19:28:16 +0200

    PLTParser: check for time dataset directly in the parser

--- src/EFA/IO/PLTImport.hs
+++ src/EFA/IO/PLTImport.hs
@@ -2,34 +2,29 @@
 
 module EFA.IO.PLTImport where
 
-import EFA.IO.PLTParser (Table, pltFile)
-import Text.ParserCombinators.Parsec (Parser, parse)
+import EFA.IO.PLTParser (Tables, pltFile)
+import Text.ParserCombinators.Parsec (parse)
 
-import EFA.Signal.Record(Record(Record),SignalRecord, SigId(SigId))
+import EFA.Signal.Record (Record(Record), SignalRecord)
 
 import qualified EFA.Signal.Signal as S
 import qualified EFA.Signal.Vector as SV
 
-import qualified Data.NonEmpty as NonEmpty
 import qualified Data.Map as Map
 
 
 makePLTRecord ::
   (SV.FromList v, SV.Storage v a) =>
-  NonEmpty.T [] (Table a) ->
-  Parser (SignalRecord v a)
-makePLTRecord (NonEmpty.Cons (SigId timeStr, time) table) =
-  case timeStr of
-    "time" ->
-      return $ Record (S.fromList time) (fmap S.fromList $ Map.fromList table)
-    _ -> fail "makePLTRecord no time"
+  Tables a -> SignalRecord v a
+makePLTRecord (time, table) =
+  Record (S.fromList time) (fmap S.fromList $ Map.fromList table)
 
 modelicaPLTImport ::
-  (SV.Storage t v, SV.FromList t, Read v) =>
-  FilePath -> IO (SignalRecord t v)
+  (SV.Storage v a, SV.FromList v, Read a) =>
+  FilePath -> IO (SignalRecord v a)
 modelicaPLTImport path = do
   text <- readFile path
-  case parse (makePLTRecord =<< pltFile) path text of
+  case parse pltFile path text of
     Left err ->
       ioError $ userError $ "Parse error in file " ++ show err
-    Right table -> return table
+    Right table -> return $ makePLTRecord table
--- src/EFA/IO/PLTParser.hs
+++ src/EFA/IO/PLTParser.hs
@@ -4,28 +4,40 @@ module EFA.IO.PLTParser where
 
 import EFA.Signal.Record (SigId(SigId))
 
-import qualified EFA.IO.Parser as EFAParser
-import EFA.IO.Parser (Sequence, number, eol)
+import EFA.IO.Parser (number, eol)
 import Text.ParserCombinators.Parsec
 import Control.Applicative ((*>), liftA2)
+import Control.Monad.HT (void)
 
 
+type Tables v = ([v], [Table v])
 type Table v = (SigId, [v])
 
 datasetToken :: Parser ()
-datasetToken = (string "DataSet:") *> spaces
+datasetToken = string "DataSet:"  *>  spaces
 
-pltFile :: (Sequence tables, Read v) => Parser (tables (Table v))
-pltFile = EFAParser.endBy table eol
+pltFile :: (Read v) => Parser (Tables v)
+pltFile = do
+   removeClutter
+   liftA2 (,) timeTable $ eol *> endBy dataTable eol
 
-table :: (Read v) => Parser (Table v)
-table = liftA2 (,) (removeClutter *> fmap SigId header) (endBy dataline eol)
+timeTable :: (Read v) => Parser [v]
+timeTable = datasetToken *> string "time\n" *> table
 
-header :: Parser String
-header = manyTill anyChar eol
+dataTable :: (Read v) => Parser (Table v)
+dataTable = liftA2 (,) (datasetToken *> fmap SigId name) table
 
-dataline :: (Read v) => Parser v
-dataline = manyTill (noneOf "\n\r") (string ", ") *> number
+table :: (Read v) => Parser [v]
+table = endBy (fmap snd dataline) eol
 
-removeClutter :: Parser String
-removeClutter = manyTill anyChar datasetToken
+name :: Parser String
+name = manyTill anyChar eol
+
+dataline :: (Read v) => Parser (v,v)
+dataline = liftA2 (,) number (string ", " *> number)
+
+removeClutter :: Parser ()
+removeClutter = void $ manyTill line (string "\n")
+
+line :: Parser String
+line = manyTill (noneOf "\n\r") eol
