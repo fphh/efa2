c2307407b8a957f467055a0895ef2d2458f47170
Henning Thielemann <git@henning-thielemann.de>
2013-06-16 17:54:46 +0200

    ASCIIImport.makeASCIIRecord: replace 'read' by a parser
    'read' may yield 'undefined', whereas the parser calls 'fail'.

--- src/EFA/IO/ASCIIImport.hs
+++ src/EFA/IO/ASCIIImport.hs
@@ -10,30 +10,44 @@ import EFA.Signal.Record (Record(Record),SignalRecord, SigId(SigId))
 import qualified EFA.Signal.Signal as S
 import qualified EFA.Signal.Vector as SV
 
+import qualified Data.Traversable as Trav
 import qualified Data.NonEmpty as NonEmpty
 import qualified Data.Zip as Zip
 import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec (parse)
+import Text.ParserCombinators.Parsec (Parser, parse)
 
+
+parseCellContent ::
+  (Read a) =>
+  String -> Parser a
+parseCellContent str =
+  case reads str of
+    [(a,"")] -> return a
+    _ -> fail $ "could not parse cell content: " ++ show str
 
 makeASCIIRecord ::
-  (SV.Storage t v, SV.FromList t, Read v) =>
+  (SV.Storage v a, SV.FromList v, Read a) =>
   NonEmpty.T [] (NonEmpty.T (NonEmpty.T []) String) ->
-  SignalRecord t v
-makeASCIIRecord hs =
-  Record (S.fromList time) (Map.fromList $ zip sigIdents $ map S.fromList sigs)
-  where sigIdents = map (SigId . ("sig_" ++) . show) [(0::Int)..]
-        NonEmpty.Cons time sigs =
-           Zip.transposeClip $ fmap (fmap read . NonEmpty.init) $
-           NonEmpty.flatten hs
+  Parser (SignalRecord v a)
+makeASCIIRecord hs = do
+  NonEmpty.Cons time sigs <-
+    Trav.mapM (Trav.mapM parseCellContent) $
+    Zip.transposeClip $ fmap NonEmpty.init $ NonEmpty.flatten hs
+  return $
+    Record
+      (S.fromList time)
+      (Map.fromList $
+       zip
+         (map (SigId . ("sig_" ++) . show) [(0::Int)..])
+         (map S.fromList sigs))
 
 -- | Main ASCII Import Function
 modelicaASCIIImport ::
-  (SV.Storage t v, SV.FromList t, Read v) =>
-  FilePath -> IO (SignalRecord t v)
+  (SV.Storage v a, SV.FromList v, Read a) =>
+  FilePath -> IO (SignalRecord v a)
 modelicaASCIIImport path = do
   text <- readFile path
-  case parse (csvFile ' ') path text of
+  case parse (makeASCIIRecord =<< csvFile ' ') path text of
     Left err ->
       ioError $ userError $ "Parse error in file " ++ show err
-    Right table -> return $ makeASCIIRecord table
+    Right table -> return table
