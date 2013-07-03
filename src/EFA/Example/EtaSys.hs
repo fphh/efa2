d67b11a6e72b4a0643b4ed34592318414e995c3c
Henning Thielemann <git@henning-thielemann.de>
2013-07-03 12:34:32 +0200

    EtaSys.storageBalance: reactivated

--- src/EFA/Example/EtaSys.hs
+++ src/EFA/Example/EtaSys.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeOperators #-}
 
 
@@ -23,7 +22,7 @@ import qualified EFA.Utility.Bifunctor as BF
 import Data.Tuple.HT (fst3, thd3)
 
 import qualified Data.Set as Set
-import qualified Data.Map as Map
+import qualified Data.Map as Map ; import Data.Map (Map)
 import qualified Data.List as List
 
 import Control.Applicative (liftA2)
@@ -41,7 +40,7 @@ lookupAbsEnergy ::
   (Ord node, Show node, Show t) =>
   String ->
   EqEnv.Complete node b (EqRec.Absolute (Result t)) ->
-  TIdx.InSection TIdx.Energy node -> Result t
+  XIdx.Energy node -> Result t
 lookupAbsEnergy caller env n = EqRec.unAbsolute $
   checkedLookup caller (EqEnv.energyMap $ EqEnv.signal env) n
 
@@ -50,7 +49,7 @@ lookupAbsPower ::
   (Ord node, Show node, Show t) =>
   String ->
   EqEnv.Complete node b (EqRec.Absolute (Result t)) ->
-  TIdx.InSection TIdx.Power node -> Result t
+  XIdx.Power node -> Result t
 lookupAbsPower caller env n = EqRec.unAbsolute $
   checkedLookup caller (EqEnv.powerMap $ EqEnv.signal env) n
 
@@ -59,7 +58,7 @@ lookupAbsEta ::
   (Ord node, Show node, Show t) =>
   String ->
   EqEnv.Complete node b (EqRec.Absolute (Result t)) ->
-  TIdx.InSection TIdx.Eta node -> Result t
+  XIdx.Eta node -> Result t
 lookupAbsEta caller env n = EqRec.unAbsolute $
   checkedLookup caller (EqEnv.etaMap $ EqEnv.signal env) n
 
@@ -72,7 +71,7 @@ etaSys ::
   Flow.RangeGraph node ->
   EqEnv.Complete node b (EqRec.Absolute (Result a)) -> Result a
 etaSys (_, topo) env = liftA2 (/) (sumRes sinks) (sumRes sources)
-  where m = Map.elems $ 
+  where m = Map.elems $
             Gr.nodeEdges $
             Gr.lefilter (TD.isStructureEdge . fst) $
             TD.dirFromSequFlowGraph topo
@@ -89,47 +88,46 @@ etaSys (_, topo) env = liftA2 (/) (sumRes sinks) (sumRes sources)
         isActiveSource (_, TD.AlwaysSource, ns) = hasStructureEdge ns
         isActiveSource (_, TD.Source, ns) = hasStructureEdge ns
         isActiveSource _ = False
- 
+
         sinkEnergies
           (TD.FlowEdge (TD.StructureEdge (TIdx.InSection sec (Gr.DirEdge a b)))) =
             Just $ lookupAbsEnergy "etaSys, sinkEnergies" env (XIdx.energy sec b a)
         sinkEnergies _ = Nothing
-            
-        sourceEnergies 
+
+        sourceEnergies
           (TD.FlowEdge (TD.StructureEdge (TIdx.InSection sec (Gr.DirEdge a b)))) =
             Just $ lookupAbsEnergy "etaSys, sourceEnergies" env (XIdx.energy sec a b)
         sourceEnergies _ = Nothing
 
-{-
--- HH: Muss repariert werden!!!
 
-
-newtype InBalance node a = InBalance (Map.Map node a) deriving (Show)
+newtype InBalance node a = InBalance (Map node a) deriving (Show)
 
 instance Functor (InBalance node) where
-         fmap f (InBalance m) = InBalance (fmap f m)
+   fmap f (InBalance m) = InBalance (fmap f m)
 
 
-newtype OutBalance node a = OutBalance (Map.Map node a) deriving (Show)
+newtype OutBalance node a = OutBalance (Map node a) deriving (Show)
 
 instance Functor (OutBalance node) where
-         fmap f (OutBalance m) = OutBalance (fmap f m)
+   fmap f (OutBalance m) = OutBalance (fmap f m)
 
 data InOutBalance node a =
-  InOutBalance (InBalance node a) (OutBalance node a) deriving (Show)
+   InOutBalance (InBalance node a) (OutBalance node a) deriving (Show)
 
 
 instance Functor (InOutBalance node) where
-         fmap f (InOutBalance ins outs) = InOutBalance (fmap f ins) (fmap f outs)
+   fmap f (InOutBalance ins outs) = InOutBalance (fmap f ins) (fmap f outs)
 
 
 {-
+siehe Graph.Flow, Graph.Draw
+-}
+
 storageBalance ::
   (Show node, Ord node, Show a) =>
   Flow.RangeGraph node ->
   EqEnv.Complete node b (EqRec.Absolute (Result a)) ->
   InOutBalance node (Result [a])
--}
 
 storageBalance (_, topo) env = InOutBalance inBalance outBalance
   where structTopo = Gr.lefilter (TD.isStructureEdge . fst) $
@@ -142,18 +140,18 @@ storageBalance (_, topo) env = InOutBalance inBalance outBalance
         outSt = concatMap (map Gr.from . Set.toList . thd3) $ filter isOutSt m
 
         -- hier soll ein Map das groupBy und sortBy ersetzen!!!
-        inStEs = map (concatMap inFunc) $ 
-                 List.groupBy (equating getNode) $ 
+        inStEs = map (concatMap inFunc) $
+                 List.groupBy (equating getNode) $
                  List.sortBy (comparing getNode) inSt
         outStEs = map (concatMap outFunc) $
                   List.groupBy (equating getNode) $
                   List.sortBy (comparing getNode) outSt
-        getNode (TIdx.ForNode _ x) = x
+        getNode (TIdx.TimeNode _ x) = x
 
         inFunc x = map (g x . getNode) $ Gr.pre structTopo x
         outFunc x = map (g x . getNode) $ Gr.suc structTopo x
 
-        g (TIdx.ForNode (TIdx.AfterSection sec) x) y =
+        g (TIdx.TimeNode (TIdx.NoInit (TIdx.NoExit sec)) x) y =
           (x, lookupAbsEnergy "storageBalance" env (XIdx.energy sec x y))
 
 
@@ -170,14 +168,14 @@ storageBalance (_, topo) env = InOutBalance inBalance outBalance
 
 
 sumBalance ::
-  (Num a) => 
+  (Num a) =>
   InOutBalance node (Result [a]) -> InOutBalance node (Result a)
 
-sumBalance bal = fmap (fmap sum) bal
+sumBalance = fmap (fmap sum)
 
 
 
-type Balance node a = Map.Map node a
+type Balance node a = Map node a
 
 
 
@@ -199,16 +197,15 @@ balance ::
   (Num d, Ord node, Show d, Show node) =>
   Flow.RangeGraph node ->
   EqEnv.Complete node b (EqRec.Absolute (Result (Data ([] :> Nil) d))) ->
-  Map.Map node (Result d)
+  Map node (Result d)
 -}
 
 balance ::
   (Num d, Ord node, Show d, Show node) =>
   Flow.RangeGraph node ->
   EqEnv.Complete node b (EqRec.Absolute (Result (Data ([] :> Nil) d))) ->
-  Map.Map node (Result d)
+  Map node (Result d)
 
 
 
 balance topo env = bal topo (BF.second (fmap (fmap (\(Data x) -> sum x))) env)
--}
