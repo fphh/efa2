ddbbf18eac007df7a71ba091163af702b5eb4801
Henning Thielemann <git@henning-thielemann.de>
2013-07-26 11:41:10 +0200

    EFA.Graph.SequenceFlow: proposal for new data structure for sequence flow graphs

---
+++ src/EFA/Graph/SequenceFlow.hs
@@ -0,0 +1,55 @@
+module EFA.Graph.SequenceFlow where
+
+import qualified EFA.Application.Index as XIdx
+
+import qualified EFA.Graph.Flow as Flow
+
+import qualified EFA.Graph.Topology as Topo
+import qualified EFA.Graph as Gr
+import EFA.Graph.Topology (FlowTopology)
+
+import qualified EFA.Signal.SequenceData as SD
+
+import qualified Data.Map as Map ; import Data.Map (Map)
+
+
+data
+   SequenceFlowGraph node structEdge nodeLabel structLabel storageLabel =
+      FlowGraph {
+         storages ::
+            Map node
+               ((nodeLabel, nodeLabel),
+                Map (XIdx.StorageEdge node) storageLabel),
+         sequ ::
+            SD.SequData
+               (Gr.Graph node structEdge nodeLabel structLabel)
+      }
+
+type
+   RangeGraph node =
+      SequenceFlowGraph
+         node Gr.EitherEdge
+         (Topo.NodeType (Maybe Topo.StoreDir)) () ()
+
+
+{-
+Alle Storages sollen in die initiale Sektion,
+auch wenn sie nie aktiv sind!
+So kann man beim Initialisieren auch Werte zuweisen.
+-}
+sequenceGraph ::
+   (Ord node) =>
+   SD.SequData (FlowTopology node) -> RangeGraph node
+sequenceGraph sd =
+   let sq = fmap Topo.classifyStorages sd
+   in  FlowGraph {
+          storages =
+             fmap
+                ((,)
+                   (Topo.Storage $ Just Topo.In,
+                    Topo.Storage $ Just Topo.Out) .
+                 Map.fromListWith (error "duplicate storage edge") .
+                 map (flip (,) ()) . Flow.storageEdges . Map.mapMaybe id) $
+             Flow.getStorageSequences sq,
+          sequ = sq
+       }
