a3741494053338fd90df768bc320f3650af54136
Philipp Guttenberg <guttenberg@energiefluss.info>
2013-07-30 14:30:16 +0200

    vergessene Module eingecheckt

---
+++ src/EFA/Application/AbsoluteState.hs
@@ -0,0 +1,219 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TypeOperators #-}
+module EFA.Application.AbsoluteState (
+   module EFA.Application.AbsoluteState,
+   (=.=),
+   ) where
+
+-- import as  import qualified EFA.Application.AbsoluteState as AbsoluteState
+
+--import qualified EFA.Application.Index as XIdx
+import qualified EFA.Application.Symbolic as Symbolic
+
+import qualified EFA.Equation.Record as Record
+--import qualified EFA.Equation.System as EqGen
+--import qualified EFA.Equation.Environment as Env
+import qualified EFA.Graph.StateFlow.EquationSystem as EqGen
+import qualified EFA.Graph.StateFlow.Environment as EqEnv
+
+--import qualified EFA.Equation.Variable as Var
+import qualified EFA.Equation.Verify as Verify
+--import EFA.Equation.System ((=.=))
+import EFA.Graph.StateFlow.EquationSystem ((=.=))
+
+import EFA.Equation.Result(Result(..))
+
+import qualified EFA.Symbolic.Variable as SymVar
+
+--import qualified EFA.Signal.Record as SigRecord
+--import qualified EFA.Signal.SequenceData as SD
+--import qualified EFA.Signal.Signal as Signal
+--import EFA.Signal.Data (Data, Nil, (:>))
+
+--import qualified EFA.Graph.Flow as Flow
+import qualified EFA.Graph.Topology.Index as Idx
+import qualified EFA.Graph.Topology.Node as Node
+import qualified EFA.Graph.Topology as TD
+
+import qualified EFA.Equation.Arithmetic as Arith
+import EFA.Utility (Pointed)
+
+import EFA.Report.FormatValue (FormatValue)
+
+import qualified UniqueLogic.ST.TF.Expression as Expr
+import qualified UniqueLogic.ST.TF.System.Simple as Sys
+
+--import qualified Data.Map as Map ;
+import Data.Map (Map)
+import Control.Applicative (liftA, liftA2)
+--import Data.Foldable (fold)
+import Data.Monoid ((<>)) --mempty
+
+
+type EquationSystem = EqGen.EquationSystem Verify.Ignore Record.Absolute
+
+type Expression node s a v x = EqGen.Expression Verify.Ignore Record.Absolute node s a v x
+
+
+solve ::
+   (Eq a, Arith.Constant a, a ~ Arith.Scalar v,
+    Eq v, Arith.Product v, Arith.Integrate v,
+    Node.C node) =>
+--   Flow.RangeGraph node ->
+   TD.StateFlowGraph node ->
+  (forall s. EquationSystem node s a v) ->
+   EqEnv.Complete node (Result a) (Result v)
+solve graph sys =
+   EqEnv.completeFMap Record.unAbsolute Record.unAbsolute $
+   EqGen.solve graph sys
+
+
+solveSimple ::
+   (Node.C node) =>
+   (forall s. EquationSystem node s a v) ->
+   EqEnv.Complete node (Result a) (Result v)
+solveSimple sys =
+   EqEnv.completeFMap Record.unAbsolute Record.unAbsolute $
+   EqGen.solveSimple sys
+
+
+fromGraph:: (Arith.Constant a, a ~ Arith.Scalar v, Eq v,
+             Eq a, Arith.Product v,
+             Arith.Product a, Arith.Integrate v, Node.C node) =>
+            Bool ->
+            TD.DirStateFlowGraph node ->
+            EquationSystem node s a v
+fromGraph = EqGen.fromGraph
+
+constant :: x -> Expression node s a v x
+constant = EqGen.constant
+
+variable ::
+   (Eq x, Arith.Sum x, x ~ EqEnv.Element idx a v,
+    EqEnv.AccessMap idx, Ord (idx node), FormatValue (idx node)) =>
+   idx node -> Expression node s a v x
+variable = EqGen.variable . Idx.absolute
+
+
+liftF ::
+  (Arith.Sum y) =>
+  (x -> y) ->
+  Expression node s a v x ->
+  Expression node s a v y
+liftF = liftA . Expr.fromRule2 . Sys.assignment2
+
+liftF2 ::
+  (Arith.Sum z) =>
+  (x -> y -> z) ->
+  Expression node s a v x ->
+  Expression node s a v y ->
+  Expression node s a v z
+liftF2 = liftA2 . Expr.fromRule3 . Sys.assignment3
+
+
+
+type SignalTerm term node = Symbolic.SignalTerm Idx.Absolute term node
+type ScalarTerm term node = Symbolic.ScalarTerm Idx.Absolute term node
+type ScalarAtom term node = Symbolic.ScalarAtom Idx.Absolute term node
+
+type VarTerm var term node = SymVar.VarTerm var Idx.Absolute term node
+
+type
+   SymbolicEquationSystem node s term =
+      Symbolic.EquationSystem Verify.Ignore Record.Absolute node s term
+
+symbol ::
+   (SymVar.Symbol var, Pointed term) =>
+   var node -> VarTerm var term node
+symbol = SymVar.symbol . Idx.absolute
+
+{-
+givenSymbol ::
+  (t ~ VarTerm var term node,
+   Eq t, Arith.Sum t,
+   t ~ EqEnv.Element idx (ScalarTerm term node) (SignalTerm term node),
+   Ord (idx node), FormatValue (idx node), Pointed term,
+   Var.Type idx ~ var, SymVar.Symbol var, EqEnv.AccessMap idx) =>
+  idx node ->
+  SymbolicEquationSystem node s term
+givenSymbol idx =
+   idx .= symbol (Var.index idx)
+-}
+{-
+infixr 6 =<>
+
+(=<>) ::
+  (t ~ VarTerm var term node,
+   Eq t, Arith.Sum t,
+   t ~ EqEnv.Element idx (ScalarTerm term node) (SignalTerm term node),
+   Ord (idx node), FormatValue (idx node), Pointed term,
+   Var.Type idx ~ var, SymVar.Symbol var, EqEnv.AccessMap idx) =>
+   idx node ->
+   SymbolicEquationSystem node s term ->
+   SymbolicEquationSystem node s term
+idx =<> eqsys = givenSymbol idx <> eqsys
+-}
+
+infix 0 .=, =%%=
+
+(.=) ::
+  (Eq x, Arith.Sum x, x ~ EqEnv.Element idx a v,
+   EqEnv.AccessMap idx, Ord (idx node), FormatValue (idx node)) =>
+   idx node -> x ->
+   EquationSystem node s a v
+evar .= val  =  variable evar =.= EqGen.constant val
+
+
+
+(=%%=) ::
+  (Eq x, Arith.Sum x, x ~ EqEnv.Element idx a v,
+  EqEnv.AccessMap idx, Ord (idx node), FormatValue (idx node)) =>
+  idx node -> idx node -> EquationSystem node s a v
+(=%%=) = (EqGen.=%%=)
+
+{-
+envFromFlowRecord ::
+   (Ord node) =>
+   SD.SequData (SigRecord.DTimeFlowRecord node v a) ->
+   EqEnv.Signal node (Data (v :> Nil) a)
+envFromFlowRecord =
+   fold .
+   SD.mapWithSection
+      (\section (SigRecord.Record times signals) ->
+         mempty {
+            EqEnv.dtimeMap =
+               Map.singleton (XIdx.dTime section) (Signal.unpack times),
+            EqEnv.powerMap =
+               Map.mapKeys
+                  (\(Idx.PPos x) -> Idx.InPart section $ Idx.Power x) $
+               fmap Signal.unpack signals
+         })
+
+-}
+fromEnvScalar ::
+   (Eq a, Arith.Sum a, Node.C node) =>
+   EqEnv.Scalar node a ->
+   EquationSystem node s a v
+fromEnvScalar = EqGen.fromEnvScalar . fmap Record.Absolute
+
+fromEnvSignal ::
+   (Eq v, Arith.Sum v, Node.C node) =>
+   EqEnv.Signal node v ->
+   EquationSystem node s a v
+fromEnvSignal = EqGen.fromEnvSignal . fmap Record.Absolute
+
+fromEnv ::
+   (Eq a, Arith.Sum a, Eq v, Arith.Sum v, Node.C node) =>
+   EqEnv.Complete node a v ->
+   EquationSystem node s a v
+fromEnv (EqEnv.Complete envScalar envSignal) =
+   fromEnvScalar envScalar <> fromEnvSignal envSignal
+
+fromMap ::
+   (Arith.Sum x, x ~ EqEnv.Element idx a v,
+    EqEnv.AccessMap idx, Ord (idx node), FormatValue (idx node)) =>
+   Map (idx node) x ->
+   EquationSystem node s a v
+fromMap =
+   EqGen.fromMap . fmap Record.Absolute
---
+++ src/EFA/Application/Simulation.hs
@@ -0,0 +1,148 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE Rank2Types #-}
+
+module EFA.Application.Simulation where
+
+--import qualified EFA.Application.Absolute as EqGen
+import qualified EFA.Application.AbsoluteState as EqGen
+
+
+import qualified EFA.Application.IndexState as XIdx
+
+import EFA.Application.AbsoluteState ( (=.=) )
+--import EFA.Application.AbsoluteState ( (=.=) )
+--import qualified EFA.Application.Utility as AppUt
+
+import qualified EFA.Signal.Data as Data
+import EFA.Signal.Data (Data(..), Nil,(:>))
+
+import qualified EFA.Graph.Topology.Index as TIdx
+--import qualified EFA.Graph.Flow as Flow
+import qualified EFA.Graph.Topology.Node as Node
+import qualified EFA.Graph.Topology as TD
+import qualified EFA.Graph.Topology.StateAnalysis as StateAnalysis
+import qualified EFA.Graph.StateFlow.Environment as EqEnv
+import qualified EFA.Graph.StateFlow as StateFlow
+
+--import qualified EFA.Equation.Environment as EqEnv
+--import qualified EFA.Equation.Record as EqRecord
+
+import qualified EFA.Signal.Vector as SV
+import qualified EFA.Signal.Base as Base
+import qualified EFA.Signal.Signal as Sig
+import qualified EFA.Signal.Record as Record
+import qualified EFA.Signal.SequenceData as SD
+
+--import qualified EFA.Application.Optimisation as AppOpt
+
+
+--import qualified EFA.Equation.Verify as Verify
+import qualified EFA.Equation.Arithmetic as EqArith
+import EFA.Equation.Result(Result)
+
+
+import qualified Data.Map as Map
+import qualified Data.Foldable as Fold
+import Data.Map (Map)
+import Data.Monoid((<>))
+
+{-
+eqs :: EqGen.EquationSystem Node s (Data Nil Double) (Data ([] :> Nil) Double)
+eqs = Simulation.givenSimulate etaAssign etaFunc $
+             SD.SequData [SD.Section (TIdx.Section 0) undefined rec]
+-}
+
+type EtaAssignMap node = Map (XIdx.Eta node) (String, String, XIdx.Eta node -> XIdx.Power node)
+
+solve :: (Node.C node,
+          Eq (v a),
+          Fractional a,
+          Ord a,
+          Show a,
+          EqArith.Constant a,
+          SV.Zipper v,
+          SV.Walker v,
+          SV.Storage v a,
+          SV.Singleton v,
+          SV.Len (v a),
+          SV.FromList v,
+          Base.BSum a) =>
+         TD.Topology node ->
+         (TIdx.State ->  EtaAssignMap node) ->
+         Map String (a -> a) ->
+         Record.PowerRecord node v a ->
+         EqEnv.Complete node (Result (Data Nil a)) (Result (Data (v :> Nil) a))
+solve topology etaAssign etaFunc powerRecord = EqGen.solveSimple $
+                                              givenSimulate stateFlowGraph etaAssign etaFunc powerRecord
+  where
+    -- | Build Sequenceflow graph for simulation
+    stateFlowGraph = StateFlow.stateGraphActualStorageEdges $ fmap (flowStates !!) $ SD.fromList [0]
+    flowStates = StateAnalysis.advanced topology
+
+
+givenSimulate ::
+ (Num a, Eq a, Show a, Fractional a, Ord a,Ord node,Node.C node,
+  Base.BSum a, EqArith.Sum a,EqArith.Constant a,
+  Eq (v a),
+  SV.Zipper v,SV.FromList v,SV.Len (v a),
+  SV.Singleton v,
+  SV.Walker v,
+  SV.Storage v a) =>
+  TD.StateFlowGraph node ->
+  (TIdx.State ->  EtaAssignMap node) ->
+  Map String (a -> a) ->
+  Record.PowerRecord node v a ->
+  (forall s . EqGen.EquationSystem node s (Data Nil a) (Data (v :> Nil) a))
+
+givenSimulate stateFlowGraph etaAssign etaFunc powerRecord =
+--  (XIdx.storage TIdx.initial Water EqGen..= Data 0) <>
+   --Fold.fold (SD.mapWithSection f sf)
+  (EqGen.fromGraph True $ TD.dirFromFlowGraph (stateFlowGraph)) <>
+   f powerRecord
+   where f (Record.Record t xs) =
+           (XIdx.dTime (TIdx.State 0) EqGen..=
+             (Data  $ SV.fromList $ replicate (Sig.len t) 1))
+           <> makeEtaFuncGiven etaAssign (TIdx.State 0) etaFunc
+           <> Fold.fold (Map.mapWithKey g xs)
+           where
+             g (TIdx.PPos (TIdx.StructureEdge p0 p1)) p =
+                   (XIdx.power (TIdx.State 0)  p0 p1 EqGen..= Sig.unpack p)
+
+-- | Generate given equations using efficiency curves or functions for a specified section
+makeEtaFuncGiven ::
+   (Fractional a, Ord a, Show a, EqArith.Sum a,
+    Data.Apply c a ~ v, Eq v, Data.ZipWith c, Data.Storage c a, Node.C node) =>
+   (TIdx.State -> EtaAssignMap node) ->
+   TIdx.State ->
+   Map String (a -> a) ->
+   EqGen.EquationSystem node s x (Data c a)
+makeEtaFuncGiven etaAssign state etaFunc = Fold.fold $ Map.mapWithKey f (etaAssign state)
+  where f n (strP, strN, g) =
+          EqGen.variable n =.= EqGen.liftF (Data.map ef) (EqGen.variable $ g n)
+          where ef x = if x >= 0 then fpos x else fneg x
+                fpos = maybe (err strP) id (Map.lookup strP etaFunc)
+                fneg = maybe (err strN) (\h -> recip . h . negate)
+                                        (Map.lookup strN etaFunc)
+                err str x = error ("not defined: " ++ show str ++ " for " ++ show x)
+
+
+{-
+-- | Generate given equations using efficiency curves or functions for a specified section
+makeEtaFuncGiven ::
+   (Fractional a, Ord a, Show a, EqArith.Sum a,
+    Data.Apply c a ~ v, Eq v, Data.ZipWith c, Data.Storage c a, Node.C node) =>
+   (TIdx.Section -> EtaAssignMap node) ->
+   TIdx.Section ->
+   Map String (a -> a) ->
+   EqGen.EquationSystem node s x (Data c a)
+makeEtaFuncGiven etaAssign sec etaFunc = Fold.fold $ Map.mapWithKey f (etaAssign sec)
+  where f n (strP, strN, g) =
+          EqGen.variable n =.= EqGen.liftF (Data.map ef) (EqGen.variable $ g n)
+          where ef x = if x >= 0 then fpos x else fneg x
+                fpos = maybe (err strP) id (Map.lookup strP etaFunc)
+                fneg = maybe (err strN) (\h -> recip . h . negate)
+                                        (Map.lookup strN etaFunc)
+                err str x = error ("not defined: " ++ show str ++ " for " ++ show x)
+
+-}                
