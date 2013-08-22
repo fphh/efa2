bef93e4cd93899b280ef2de62f567490a0802860
Henning Thielemann <git@henning-thielemann.de>
2013-08-22 11:00:41 +0200

    Flow.Draw.topology, topologyWithEdgeLabels, flowTopologies

--- src/EFA/Flow/Draw.hs
+++ src/EFA/Flow/Draw.hs
@@ -5,31 +5,35 @@ module EFA.Flow.Draw (
   title, bgcolour,
 
   cumulatedFlow,
+
+  topologyWithEdgeLabels,
+  topology,
+  flowTopologies,
   ) where
 
 import qualified EFA.Flow.Cumulated.Quantity as CumFlowQuant
 
 import qualified EFA.Report.Format as Format
 import EFA.Report.FormatValue (FormatValue, formatValue)
-import EFA.Report.Format (Unicode(unUnicode))
+import EFA.Report.Format (Unicode(Unicode, unUnicode))
 
 import qualified EFA.Graph.Topology.Index as Idx
 import qualified EFA.Graph.Topology.Node as Node
 import qualified EFA.Graph.Topology as Topo
 import qualified EFA.Graph as Gr
+import EFA.Graph.Topology (FlowTopology)
 import EFA.Graph (DirEdge(DirEdge))
 
 import Data.GraphViz (
-          runGraphvizCanvas,
-          GraphvizCanvas(Xlib),
-          runGraphvizCommand,
+          GraphvizCanvas(Xlib), runGraphvizCanvas,
+          GraphvizCommand(Dot), runGraphvizCommand,
+          GraphvizOutput(..),
           GraphID(Int),
           GlobalAttributes(GraphAttrs),
-          GraphvizCommand(Dot),
-          GraphvizOutput(..),
           DotEdge(DotEdge),
           DotGraph(DotGraph),
           DotNode(DotNode),
+          DotSubGraph(DotSG),
           DotStatements(DotStmts),
           attrStmts, nodeStmts, edgeStmts, graphStatements,
           directedGraph, strictGraph, subGraphs,
@@ -48,7 +52,11 @@ import qualified Data.Accessor.Basic as Accessor
 import qualified Data.Text.Lazy as T
 
 import qualified Data.Foldable as Fold
+import qualified Data.Map as Map
 import qualified Data.List as List
+
+import Data.Map (Map)
+import Data.Foldable (Foldable, fold)
 
 import Control.Monad (void)
 import Control.Category ((.))
@@ -139,6 +147,83 @@ dotIdentFromNode :: (Node.C node) => node -> T.Text
 dotIdentFromNode n = T.pack $ Node.dotId n
 
 
+topology :: (Node.C node) => Topo.Topology node -> DotGraph T.Text
+topology topo = dotFromTopology Map.empty topo
+
+topologyWithEdgeLabels ::
+   (Node.C node) =>
+   Map (node, node) String -> Topo.Topology node -> DotGraph T.Text
+topologyWithEdgeLabels edgeLabels topo =
+   dotFromTopology edgeLabels topo
+
+dotFromTopology ::
+   (Node.C node) =>
+   Map (node, node) String ->
+   Topo.Topology node -> DotGraph T.Text
+dotFromTopology edgeLabels g =
+   DotGraph {
+      strictGraph = False,
+      directedGraph = False,
+      graphID = Just (Int 1),
+      graphStatements =
+         DotStmts {
+            attrStmts = [],
+            subGraphs = [],
+            nodeStmts = map dotFromTopoNode $ Gr.labNodes g,
+            edgeStmts = map (dotFromTopoEdge edgeLabels) $ Gr.edges g
+         }
+   }
+
+dotFromTopoNode ::
+  (Node.C node, StorageLabel store) =>
+  Gr.LNode node (Topo.NodeType store) -> DotNode T.Text
+dotFromTopoNode (x, typ) =
+  DotNode
+    (dotIdentFromNode x)
+    (nodeAttrs typ $ labelFromUnicode $ Node.display x)
+
+dotFromTopoEdge ::
+  (Node.C node) =>
+  Map (node, node) String ->
+  DirEdge node -> DotEdge T.Text
+dotFromTopoEdge edgeLabels e =
+  case orientDirEdge e of
+     (DirEdge x y, _, _) ->
+           let lab = T.pack $ fold $ Map.lookup (x, y) edgeLabels
+           in  DotEdge
+                 (dotIdentFromNode x)
+                 (dotIdentFromNode y)
+                 [ Viz.Dir Viz.NoDir, structureEdgeColour,
+                   Viz.Label $ Viz.StrLabel lab, Viz.EdgeTooltip lab ]
+
+
+flowTopologies ::
+   (Node.C node) =>
+   [FlowTopology node] -> DotGraph T.Text
+flowTopologies ts = DotGraph False True Nothing stmts
+   where stmts = DotStmts attrs subgs [] []
+         subgs = zipWith dotFromFlowTopology [0..] ts
+         attrs = []
+
+dotFromFlowTopology ::
+   (Node.C node) =>
+   Int -> FlowTopology node -> DotSubGraph T.Text
+dotFromFlowTopology ident topo =
+   DotSG True (Just (Int ident)) $
+   DotStmts
+      [GraphAttrs [labelFromString $ show ident]] []
+      (map mkNode $ Gr.labNodes topo)
+      (map mkEdge $ Gr.edges topo)
+  where idf x = T.pack $ show ident ++ "_" ++ Node.dotId x
+        mkNode x@(n, t) =
+           DotNode (idf n)
+              (nodeAttrs t $ labelFromUnicode $ formatTypedNode x)
+        mkEdge el =
+           case orientEdge el of
+              (DirEdge x y, d, _) ->
+                 DotEdge (idf x) (idf y) [Viz.Dir d]
+
+
 class Reverse s where
    reverse :: s -> s
 
@@ -148,6 +233,17 @@ instance Reverse [s] where
 
 data Order = Id | Reverse deriving (Eq, Show)
 
+
+orientEdge ::
+   (Ord node) =>
+   Gr.EitherEdge node -> (DirEdge node, Viz.DirType, Order)
+orientEdge e =
+   case e of
+      Gr.EUnDirEdge ue -> (orientUndirEdge ue, Viz.NoDir, Id)
+      Gr.EDirEdge de -> orientDirEdge de
+
+orientUndirEdge :: Ord node => Gr.UnDirEdge node -> DirEdge node
+orientUndirEdge (Gr.UnDirEdge x y) = DirEdge x y
 
 orientDirEdge ::
    (Ord node) =>
@@ -168,6 +264,26 @@ instance StorageLabel () where
 instance Show a => StorageLabel (Maybe a) where
    formatStorageLabel Nothing = ""
    formatStorageLabel (Just dir) = " " ++ show dir
+
+
+showType :: StorageLabel store => Topo.NodeType store -> String
+showType typ =
+   case typ of
+      Topo.Storage store -> "Storage" ++ formatStorageLabel store
+      Topo.Sink          -> "Sink"
+      Topo.AlwaysSink    -> "AlwaysSink"
+      Topo.Source        -> "Source"
+      Topo.AlwaysSource  -> "AlwaysSource"
+      Topo.Crossing      -> "Crossing"
+      Topo.DeadNode      -> "DeadNode"
+      Topo.NoRestriction -> "NoRestriction"
+
+
+formatTypedNode ::
+   (Node.C node, StorageLabel store) =>
+   (node, Topo.NodeType store) -> Unicode
+formatTypedNode (n, l) =
+   Unicode $ unUnicode (Node.display n) ++ " - " ++ showType l
 
 
 cumulatedFlow ::
