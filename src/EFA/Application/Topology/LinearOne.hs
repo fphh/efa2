0e4323d17c6577eaececb87bc603503c63da54b1
Henning Thielemann <git@henning-thielemann.de>
2013-09-13 11:29:56 +0200

    Topology.LinearOne: format subscripts as 0 and 1

--- src/EFA/Application/Topology/LinearOne.hs
+++ src/EFA/Application/Topology/LinearOne.hs
@@ -5,12 +5,15 @@ import EFA.Application.Utility ( topologyFromEdges )
 import qualified EFA.Graph.Topology.Node as Node
 import qualified EFA.Graph.Topology as Topo
 
+import qualified EFA.Report.Format as Format
+
 
 data Node = Sink | Source deriving (Ord, Eq, Enum, Show)
 
 instance Node.C Node where
    display = Node.displayDefault
-   subscript = Node.subscriptDefault
+   subscript Source = Format.integer 0
+   subscript Sink = Format.integer 1
    dotId = Node.dotIdDefault
 
    typ Source = Node.AlwaysSource
