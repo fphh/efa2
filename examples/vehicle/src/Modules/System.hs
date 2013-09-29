232471e985b26181ec14e6c1cea54e4714072544
Henning Thielemann <git@henning-thielemann.de>
2013-09-29 11:40:15 +0200

    Modules.System.showPowerId, convertPowerId: simplified

--- examples/vehicle/src/Modules/System.hs
+++ examples/vehicle/src/Modules/System.hs
@@ -80,14 +80,9 @@ powerPositonNames = Map.fromList $ concat $ map f edgeList
                              (SeqIdx.ppos n2 n1, SigId $ "Power-"++l2)]
 
 showPowerId :: SeqIdx.PPos Node -> String
-showPowerId ppos = f (Map.lookup  ppos powerPositonNames)
-  where
-    f (Just sid) = show sid
-    f Nothing = (show ppos)
-
+showPowerId ppos =
+  maybe (show ppos) show $ Map.lookup  ppos powerPositonNames
 
 convertPowerId :: SeqIdx.PPos Node -> SigId
-convertPowerId ppos =  f (Map.lookup  ppos powerPositonNames)
-  where
-    f (Just sid) = sid
-    f Nothing = SigId (show ppos)
+convertPowerId ppos =
+  Map.findWithDefault (SigId $ show ppos) ppos powerPositonNames
