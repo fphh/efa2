ee55b34b04396c3e5036d446c629b09bbd8fa605
Henning Thielemann <git@henning-thielemann.de>
2013-10-12 21:01:10 +0200

    demo/functionEta: use topology instead of sequence flow graph for computation

--- demo/functionEta/Main.hs
+++ demo/functionEta/Main.hs
@@ -3,12 +3,12 @@ module Main where
 
 import qualified EFA.Example.Topology.LinearOne as LinearOne
 import EFA.Example.Topology.LinearOne (Node(Sink, Source))
-import EFA.Application.Utility (seqFlowGraphFromTopology)
+import EFA.Application.Utility (quantityTopology)
 
-import qualified EFA.Flow.Sequence.Absolute as EqSys
-import qualified EFA.Flow.Sequence.Quantity as SeqFlow
-import qualified EFA.Flow.Sequence.Index as XIdx
-import EFA.Flow.Sequence.Absolute ((.=), (=.=))
+import qualified EFA.Flow.Topology.Absolute as EqSys
+import qualified EFA.Flow.Topology.Quantity as FlowTopo
+import qualified EFA.Flow.Topology.Index as XIdx
+import EFA.Flow.Topology.Absolute ((.=), (=.=))
 
 import qualified EFA.Equation.Variable as Var
 import EFA.Equation.Result (Result)
@@ -24,45 +24,39 @@ import EFA.Report.FormatValue (formatValue)
 import Data.Monoid (mconcat, (<>))
 
 
-sec0 :: Idx.Section
-sec0 :~ _ = Stream.enumFrom $ Idx.Section 0
-
-
-flowGraph :: SeqFlow.Graph Node (Result a) (Result v)
-flowGraph = seqFlowGraphFromTopology LinearOne.topology
-
 enRange :: [Double]
 enRange = 0.01:[0.5, 1 .. 9]
 
 
-type Expr s a = EqSys.ExpressionIgnore Node s Double Double a
+type Expr s a = EqSys.ExpressionIgnore Node s Double a
 
 c :: XIdx.Power Node
-c = XIdx.power sec0 Source Sink
+c = XIdx.power Source Sink
 
 eta :: XIdx.Eta Node
-eta = XIdx.eta sec0 Source Sink
+eta = XIdx.eta Source Sink
 
 
 functionEta :: Expr s Double -> Expr s Double
 functionEta = EqSys.liftF $ \p -> 0.3 * sqrt p
 
-given :: Double -> EqSys.EquationSystemIgnore Node s Double Double
+given :: Double -> EqSys.EquationSystemIgnore Node s Double
 given p =
    mconcat $
-   (XIdx.dTime sec0 .= 1) :
-   (XIdx.power sec0 Source Sink .= p) :
+   (XIdx.dTime .= 1) :
+   (XIdx.power Source Sink .= p) :
    []
 
 
 solve :: Double -> String
 solve p =
    let env =
-          EqSys.solve flowGraph
+          EqSys.solve
+             (quantityTopology LinearOne.topology)
              ((EqSys.variable eta =.= functionEta (EqSys.variable c)) <> given p)
    in  show p ++ " " ++
        Format.unUnicode (formatValue
-          (Var.checkedLookup "solve" SeqFlow.lookupEta eta env))
+          (Var.checkedLookup "solve" FlowTopo.lookupEta eta env))
 
 main :: IO ()
 main =
