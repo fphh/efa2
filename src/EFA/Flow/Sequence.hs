805c7d07523cec3eacaf19ca867bcd92f8d8269e
Henning Thielemann <git@henning-thielemann.de>
2013-10-19 20:38:24 +0200

    Flow.Sequence.storageEdges, storageSequences: taken from Graph.Flow

--- src/EFA/Flow/Sequence.hs
+++ src/EFA/Flow/Sequence.hs
@@ -5,8 +5,6 @@ import qualified EFA.Flow.Topology as FlowTopo
 import qualified EFA.Flow.Storage as Storage
 import qualified EFA.Flow.PartMap as PartMap
 
-import qualified EFA.Graph.Flow as Flow
-
 import qualified EFA.Graph.Topology.Index as Idx
 import qualified EFA.Graph.Topology.Node as Node
 import qualified EFA.Graph.Topology as Topo
@@ -67,8 +65,8 @@ sequenceGraph sd =
           storages =
              fmap
                 (storageMapFromList (Fold.toList $ Sequ.mapWithSection const sq) .
-                 Flow.storageEdges . Map.mapMaybe id) $
-             Flow.getStorageSequences sq,
+                 storageEdges . Map.mapMaybe id) $
+             storageSequences sq,
           sequence = Sequ.toMap $ fmap (FlowTopo.Section ()) sq
        }
 
@@ -84,6 +82,33 @@ storageMapFromList secs edges =
        map (flip (,) ()) edges),
     Map.fromList $ map (flip (,) () . Idx.Following) $
     Idx.Init : map Idx.NoInit secs)
+
+storageEdges ::
+   Map Idx.Section Topo.StoreDir -> [Idx.StorageEdge Idx.Section node]
+storageEdges stores = do
+   let (ins, outs) = Map.partition (Topo.In ==) stores
+   secin <- Idx.Init : map Idx.NoInit (Map.keys ins)
+   secout <-
+      (++[Idx.Exit]) $ map Idx.NoExit $ Map.keys $
+      case secin of
+         Idx.Init -> outs
+         Idx.NoInit s -> snd $ Map.split s outs
+   return $ Idx.StorageEdge secin secout
+
+storageSequences ::
+   (Ord node) =>
+   Sequ.List (Topo.ClassifiedTopology node) ->
+   Map node (Map Idx.Section (Maybe Topo.StoreDir))
+storageSequences =
+   Map.unionsWith (Map.unionWith (error "duplicate section for node"))
+   .
+   Fold.toList
+   .
+   Sequ.mapWithSection
+      (\s g ->
+         fmap (Map.singleton s) $
+         Map.mapMaybe Topo.maybeStorage $ Graph.nodeLabels g)
+
 
 groupEdges ::
    (Ord part, Ord node) =>
