0a99ecd28ca95c037cd8d9149d3bd0991276b6e4
Henning Thielemann <git@henning-thielemann.de>
2013-10-30 13:27:54 +0100

    Signal.Base.signExact

--- src/EFA/Signal/Base.hs
+++ src/EFA/Signal/Base.hs
@@ -3,24 +3,18 @@ module EFA.Signal.Base where
 import qualified EFA.Equation.Arithmetic as Arith
 
 
--- Own User Defined Sign Variable
-data Sign = PSign
-          | ZSign
-          | NSign deriving (Show, Eq, Enum)
--- data Sign = PSign | ZSign | NSign deriving (Show, Eq, Ord)
+data Sign = PSign | ZSign | NSign deriving (Show, Eq, Enum)
 
 
--- | determine Signal Sign
 sign :: (Ord a, Arith.Constant a) => a -> Sign
 sign x | x > eps = PSign
        | x < Arith.negate eps = NSign
        | otherwise = ZSign
        where eps = Arith.zero -- eps = 1^^(-12::Int) -- eps = 1^^(-1::Int)
 
-{-
-sign x =
-   case compare x 0 of
+signExact :: (Ord a, Arith.Constant a) => a -> Sign
+signExact x =
+   case compare x Arith.zero of
       GT -> PSign
       EQ -> ZSign -- TODO add intervals later on Zero - Detection
       LT -> NSign
--}
