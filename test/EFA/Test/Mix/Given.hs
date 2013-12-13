ba4ff58df08859f7ffba7e703bf7e1ddbc887fad
Henning Thielemann <git@henning-thielemann.de>
2013-12-13 22:30:19 +0100

    Test.Mix.Given.fullGiven: complete equations for all variables

--- test/EFA/Test/Mix/Given.hs
+++ test/EFA/Test/Mix/Given.hs
@@ -212,27 +212,57 @@ partialEquations =
    []
 
 
+infixl 1 #
+
+(#) :: a -> (a,a) -> Mix a
+(#) total (x,y) = Record.Mix total $ x !: y !: FL.end
+
+
+infix 0 %=
+
+(%=) ::
+   (Arith.Constant x, Verify.LocalVar mode x, FlowTopo.Lookup idx) =>
+   idx Node -> Mix (Mix Rational) ->
+   EqSys.EquationSystem mode MultiMix Node s x
+evar %= val  =
+   evar EqSys.%= fmap Arith.fromRational (Record.ExtMix val)
+
+
 fullGiven :: EquationSystem s
 fullGiven = mconcat $
-   (idxMultiMixTotal XIdx.dTime .= 0.5) :
-
-   (idxMultiMixTotal (XIdx.power source0 crossing) .= 4) :
-   (idxMultiMix idxMix1 idxMix0 (XIdx.power source0 crossing) .= 0) :
-   (idxMultiMix idxMix1 idxMix1 (XIdx.power source0 crossing) .= 0) :
-
-   (idxMultiMixTotal (XIdx.power source1 crossing) .= 3) :
-   (idxMultiMix idxMix0 idxMix0 (XIdx.power source1 crossing) .= 0) :
-   (idxMultiMix idxMix0 idxMix1 (XIdx.power source1 crossing) .= 0) :
-
-   (idxMultiMixTotal (XIdx.power sink crossing) .= 0.4) :
-   (idxMultiMix idxMix0 idxMix1 (XIdx.power sink crossing) .= 0) :
-   (idxMultiMix idxMix1 idxMix1 (XIdx.power sink crossing) .= 0) :
-
-   (idxMultiMix idxMix0 idxMix0 (XIdx.power storage crossing) .= 0) :
-   (idxMultiMix idxMix1 idxMix0 (XIdx.power storage crossing) .= 0) :
-
-   (idxMultiMixTotal (XIdx.eta source0 crossing) .= 0.25) :
-   (idxMultiMixTotal (XIdx.eta source1 crossing) .= 0.5) :
-   (idxMultiMixTotal (XIdx.eta crossing storage) .= 0.75) :
-   (idxMultiMixTotal (XIdx.eta crossing sink) .= 0.8) :
+   (XIdx.dTime %= 1/2 # (1/2,1/2) # (1/2 # (1/2,1/2),1/2 # (1/2,1/2))) :
+   (XIdx.energy source0 crossing %= 2 # (2/5,8/5) # (2 # (2/5,8/5),0 # (0,0))) :
+   (XIdx.energy source1 crossing %= 3/2 # (3/10,6/5) # (0 # (0,0),3/2 # (3/10,6/5))) :
+   (XIdx.energy crossing source0 %= 1/2 # (1/10,2/5) # (1/2 # (1/10,2/5),0 # (0,0))) :
+   (XIdx.energy crossing source1 %= 3/4 # (3/20,3/5) # (0 # (0,0),3/4 # (3/20,3/5))) :
+   (XIdx.energy crossing storage %= 1 # (0,1) # (2/5 # (0,2/5),3/5 # (0,3/5))) :
+   (XIdx.energy crossing sink %= 1/4 # (1/4,0) # (1/10 # (1/10,0),3/20 # (3/20,0))) :
+   (XIdx.energy storage crossing %= 3/4 # (0,3/4) # (3/10 # (0,3/10),9/20 # (0,9/20))) :
+   (XIdx.energy sink crossing %= 1/5 # (1/5,0) # (2/25 # (2/25,0),3/25 # (3/25,0))) :
+   (XIdx.power source0 crossing %= 4 # (4/5,16/5) # (4 # (4/5,16/5),0 # (0,0))) :
+   (XIdx.power source1 crossing %= 3 # (3/5,12/5) # (0 # (0,0),3 # (3/5,12/5))) :
+   (XIdx.power crossing source0 %= 1 # (1/5,4/5) # (1 # (1/5,4/5),0 # (0,0))) :
+   (XIdx.power crossing source1 %= 3/2 # (3/10,6/5) # (0 # (0,0),3/2 # (3/10,6/5))) :
+   (XIdx.power crossing storage %= 2 # (0,2) # (4/5 # (0,4/5),6/5 # (0,6/5))) :
+   (XIdx.power crossing sink %= 1/2 # (1/2,0) # (1/5 # (1/5,0),3/10 # (3/10,0))) :
+   (XIdx.power storage crossing %= 3/2 # (0,3/2) # (3/5 # (0,3/5),9/10 # (0,9/10))) :
+   (XIdx.power sink crossing %= 2/5 # (2/5,0) # (4/25 # (4/25,0),6/25 # (6/25,0))) :
+   (XIdx.eta source0 crossing %= 1/4 # (1/4,1/4) # (1/4 # (1/4,1/4),1/4 # (1/4,1/4))) :
+   (XIdx.eta source1 crossing %= 1/2 # (1/2,1/2) # (1/2 # (1/2,1/2),1/2 # (1/2,1/2))) :
+   (XIdx.eta crossing storage %= 3/4 # (3/4,3/4) # (3/4 # (3/4,3/4),3/4 # (3/4,3/4))) :
+   (XIdx.eta crossing sink %= 4/5 # (4/5,4/5) # (4/5 # (4/5,4/5),4/5 # (4/5,4/5))) :
+   (XIdx.x source0 crossing %= 1 # (1,1) # (1 # (1,1),1 # (1,1))) :
+   (XIdx.x source1 crossing %= 1 # (1,1) # (1 # (1,1),1 # (1,1))) :
+   (XIdx.x crossing source0 %= 2/5 # (2/5,2/5) # (1 # (1,1),0 # (0,0))) :
+   (XIdx.x crossing source1 %= 3/5 # (3/5,3/5) # (0 # (0,0),1 # (1,1))) :
+   (XIdx.x crossing storage %= 4/5 # (0,1) # (4/5 # (0,1),4/5 # (0,1))) :
+   (XIdx.x crossing sink %= 1/5 # (1,0) # (1/5 # (1,0),1/5 # (1,0))) :
+   (XIdx.x storage crossing %= 1 # (1,1) # (1 # (1,1),1 # (1,1))) :
+   (XIdx.x sink crossing %= 1 # (1,1) # (1 # (1,1),1 # (1,1))) :
+   (XIdx.inSum crossing %= 5/4 # (1/4,1) # (1/2 # (1/10,2/5),3/4 # (3/20,3/5))) :
+   (XIdx.inSum storage %= 3/4 # (0,3/4) # (3/10 # (0,3/10),9/20 # (0,9/20))) :
+   (XIdx.inSum sink %= 1/5 # (1/5,0) # (2/25 # (2/25,0),3/25 # (3/25,0))) :
+   (XIdx.outSum source0 %= 2 # (2/5,8/5) # (2 # (2/5,8/5),0 # (0,0))) :
+   (XIdx.outSum source1 %= 3/2 # (3/10,6/5) # (0 # (0,0),3/2 # (3/10,6/5))) :
+   (XIdx.outSum crossing %= 5/4 # (1/4,1) # (1/2 # (1/10,2/5),3/4 # (3/20,3/5))) :
    []
