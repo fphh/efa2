63383677cafa92607161acf7a3c35414b5a9d2ab
Henning Thielemann <git@henning-thielemann.de>
2013-12-20 12:21:35 +0100

    Cumulated.Index: helper functions for index definition

--- src/EFA/Flow/Cumulated/Index.hs
+++ src/EFA/Flow/Cumulated/Index.hs
@@ -9,6 +9,12 @@ module EFA.Flow.Cumulated.Index (
    Direction(..),
    Graph.DirEdge(..),
    TopoIdx.formatDirection,
+
+   inEnergy, outEnergy,
+   inPower, outPower,
+   inX, outX,
+   eta, dTime,
+   TopoIdx.inSum, TopoIdx.outSum,
    ) where
 
 import qualified EFA.Flow.Topology.Index as TopoIdx
@@ -26,3 +32,29 @@ data X node = X Direction (Graph.DirEdge node) deriving (Show, Ord, Eq)
 newtype Eta node = Eta (Graph.DirEdge node) deriving (Show, Ord, Eq)
 
 newtype DTime node = DTime (Graph.DirEdge node) deriving (Show, Ord, Eq)
+
+
+edge ::
+   (Graph.DirEdge node -> idx) ->
+   node -> node -> idx
+edge mkIdx from to = mkIdx $ Graph.DirEdge from to
+
+
+inEnergy, outEnergy :: node -> node -> Energy node
+inEnergy  = edge (Energy In)
+outEnergy = edge (Energy Out)
+
+inPower, outPower :: node -> node -> Power node
+inPower  = edge (Power In)
+outPower = edge (Power Out)
+
+inX, outX :: node -> node -> X node
+inX  = edge (X In)
+outX = edge (X Out)
+
+
+eta :: node -> node -> Eta node
+eta = edge Eta
+
+dTime :: node -> node -> DTime node
+dTime = edge DTime
