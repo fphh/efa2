724b679a9997646a0d916d76e004fd9982048864
Henning Thielemann <git@henning-thielemann.de>
2013-12-21 02:03:24 +0100

    Test.Mix.plausibility: check ordering conditions on mix totals and mix components

--- test/EFA/Test/Mix.hs
+++ test/EFA/Test/Mix.hs
@@ -4,13 +4,18 @@ module EFA.Test.Mix where
 import qualified EFA.TestUtility as Test
 
 import qualified EFA.Test.Mix.Given as Given
+import EFA.Example.Record.SourceSinkMix (MultiMix)
+
+import EFA.Application.Utility (checkDetermined)
 
 import qualified EFA.Flow.Topology.EquationSystem as EqSys
 import qualified EFA.Flow.Topology.AssignMap as AssignMap
 import qualified EFA.Flow.Topology.Quantity as FlowTopo
+import qualified EFA.Flow.Topology as FlowTopoPlain
 import qualified EFA.Flow.Draw as Draw
 
 import qualified EFA.Graph.Topology.Node as Node
+import qualified EFA.Graph as Graph
 
 import qualified EFA.Equation.Verify as Verify
 import qualified EFA.Equation.RecordIndex as RecIdx
@@ -21,6 +26,7 @@ import qualified EFA.Report.Format as Format
 import EFA.Report.FormatValue (FormatValue, formatValue)
 import EFA.Report.Format (Format)
 
+import qualified EFA.Utility.FixedLength as FixedLength
 import EFA.Utility.Async (concurrentlyMany_)
 
 import qualified Control.Monad.Exception.Synchronous as ME
@@ -29,10 +35,12 @@ import System.Exit (exitFailure)
 
 import qualified Data.Foldable as Fold
 import qualified Data.List.HT as ListHT
+import qualified Data.NonEmpty as NonEmpty
 import Control.Monad (when)
 import Control.Applicative (liftA2)
-import Data.Monoid (All(All), getAll)
-import Data.Foldable (forM_, foldMap)
+import Data.Traversable (sequenceA)
+import Data.Foldable (Foldable, forM_, foldMap)
+import Data.Monoid (All(All), getAll, (<>))
 
 
 completelyDetermined ::
@@ -86,6 +94,98 @@ minimalDeterminateness =
       "at least one of the following equations is redundant: " ++
       show redundantEqs
     return minimal
+
+
+foldAll :: (Foldable f) => (a -> Bool) -> f a -> All
+foldAll f = foldMap (All . f)
+
+uniformMix :: (Foldable f, Ord a) => Record.Mix dir f a -> All
+uniformMix (Record.Mix s ps)  =  foldAll (s==) ps
+
+enclosingMix :: (Foldable f, Ord a) => Record.Mix dir f a -> All
+enclosingMix (Record.Mix s ps) =
+  All $
+  NonEmpty.minimum ps <= s  &&  s <= NonEmpty.maximum ps
+
+boundedMix :: (Foldable f, Ord a) => Record.Mix dir f a -> All
+boundedMix (Record.Mix s ps)  =  foldAll (s>=) ps
+
+testDTime :: (Ord a) => MultiMix a -> All
+testDTime m =
+  foldMap uniformMix (Record.getExtMix m)
+  <>
+  foldMap uniformMix (sequenceA $ Record.getExtMix m)
+
+testSumsInner ::
+  (Ord a, FixedLength.C f0, Foldable f1) =>
+  FlowTopo.Sums (Record.ExtMix dir0 f0 (Record.Mix dir1 f1) a) -> All
+testSumsInner
+    (FlowTopo.Sums {
+      FlowTopo.sumIn = sumIn,
+      FlowTopo.sumOut = sumOut
+    }) =
+  foldMap (foldMap boundedMix . Record.getExtMix) sumOut
+  <>
+  foldMap (foldMap boundedMix . Record.getExtMix) sumIn
+
+transposeMix ::
+  (FixedLength.C len0, FixedLength.C len1) =>
+  Record.ExtMix dir0 len0 (Record.Mix dir1 len1) a ->
+  Record.ExtMix dir1 len1 (Record.Mix dir0 len0) a
+transposeMix =
+  Record.ExtMix . sequenceA . Record.getExtMix
+
+testSums :: (Ord a) => FlowTopo.Sums (MultiMix a) -> All
+testSums m =
+  testSumsInner m
+  <>
+  testSumsInner (fmap transposeMix m)
+
+testFlowInner ::
+  (Ord a, FixedLength.C f0, Foldable f1) =>
+  FlowTopo.Flow (Record.ExtMix dir0 f0 (Record.Mix dir1 f1) a) -> All
+testFlowInner
+    (FlowTopo.Flow {
+      FlowTopo.flowXOut = xout,
+      FlowTopo.flowEnergyOut = eout,
+      FlowTopo.flowPowerOut = pout,
+      FlowTopo.flowXIn = xin,
+      FlowTopo.flowEnergyIn = ein,
+      FlowTopo.flowPowerIn = pin,
+      FlowTopo.flowEta = eta
+    }) =
+  foldMap boundedMix (Record.getExtMix eout)
+  <>
+  foldMap boundedMix (Record.getExtMix ein)
+  <>
+  foldMap boundedMix (Record.getExtMix pout)
+  <>
+  foldMap boundedMix (Record.getExtMix pin)
+  <>
+  foldMap enclosingMix (Record.getExtMix xout)
+  <>
+  foldMap enclosingMix (Record.getExtMix xin)
+  <>
+  foldMap enclosingMix (Record.getExtMix eta)
+
+testFlow :: (Ord a) => FlowTopo.Flow (MultiMix a) -> All
+testFlow m =
+  testFlowInner m
+  <>
+  testFlowInner (fmap transposeMix m)
+
+plausibility :: IO ()
+plausibility =
+  Test.singleIO "Check plausibility of computed mixes." $ do
+  FlowTopoPlain.Section dtime topo <-
+    fmap (FlowTopo.mapSection (fmap (checkDetermined "plausibility"))) $
+    checkException Given.solvedGraph
+  return $ getAll $
+    testDTime dtime
+    <>
+    (foldMap testSums $ Graph.nodeLabels topo)
+    <>
+    (foldMap (foldMap testFlow) $ Graph.edgeLabels topo)
 
 
 checkException ::
@@ -159,6 +259,7 @@ runTests = do
   minimalDeterminateness
   correctness
   consistency
+  plausibility
   putStrLn ""
 
 
