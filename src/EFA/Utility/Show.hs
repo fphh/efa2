638ec260d127c304245ca803866c24b2f59158c8
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2014-01-23 15:29:48 +0100

    Show initiale Version

---
+++ src/EFA/Utility/Show.hs
@@ -0,0 +1,17 @@
+
+
+
+module EFA.Utility.Show where
+ 
+import qualified EFA.Report.Format as Format
+
+import qualified EFA.Flow.Topology.Index as TopoIdx
+
+import qualified EFA.Graph.Topology.Node as Node
+
+showNode :: (Node.C node) => node -> String
+showNode node = Format.showRaw (Node.display node :: Format.ASCII)
+
+showEdge :: (Node.C node) => TopoIdx.Position node -> String
+showEdge (TopoIdx.Position a b) = showNode a ++ arrow ++ showNode b
+  where arrow = " -> "
