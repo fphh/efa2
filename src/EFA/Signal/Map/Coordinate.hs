1096daead3d0d5a8ae256aa86ffae39d281b0006
Philipp Guttenberg <guttenberg@energiefluss.info>
2014-03-20 22:14:52 +0100

    Umbennenung von Ortho nach Cube und von Axes nach Coordinate, Axes nach Cpoordinte.System und Einfuehrung von Coordinate.Point, loeschung alter Dateien

--- src/EFA/Signal/Map/Coordinate.hs
+++ src/EFA/Signal/Map/Coordinate.hs
@@ -1,21 +1,34 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module EFA.Signal.Map.Axes where
+module EFA.Signal.Map.Coordinate where
 
 import EFA.Utility(Caller)
 import qualified EFA.Signal.Vector as SV
 
-import qualified EFA.Signal.Map.Dimension as Dim
+import qualified EFA.Signal.Map.Dimension as Dim 
 
 
--- | Datatype with monotonically rising values
+-- | Datatype with monotonically rising values 
 newtype Axis vec a = Axis {getVec :: vec a} deriving (Show,Eq)
 -- newtype Data vec a = Data (vec a) deriving Show
 
 newtype Idx = Idx {getInt :: Int} deriving Show
 
+type System dim vec a = Dim.Data dim (Axis vec a)
+data Point dim a = Point (Dim.Data dim a) deriving Show
+
+pointFromList :: [a] -> Point dim a
+pointFromList xs = Point $ Dim.Data xs
+
+pointtoList :: Point dim a -> [a]
+pointtoList (Point (Dim.Data xs)) = xs
+
+mapPoint :: (a -> b) -> Point dim a -> Point dim b
+mapPoint f (Point (Dim.Data xs)) = Point $ Dim.Data $ map f xs
+
+
 indexAdd :: Idx -> Int -> Idx
-indexAdd (Idx idx) num = Idx $ (idx+num)
+indexAdd (Idx idx) num = Idx $ (idx+num) 
 
 type DimIdx dim = Dim.Data dim Idx
 
@@ -24,54 +37,51 @@ len ::
   Axis vec a -> Int
 len (Axis vec) = SV.length vec
 
-fromVec ::
-  (SV.Storage vec Bool, SV.Singleton vec,
+fromVec :: 
+  (SV.Storage vec Bool, SV.Singleton vec, 
    SV.Zipper vec, SV.Storage vec a,Ord a) =>
   Caller -> vec a -> Axis vec a
-fromVec caller vec =
-  if isMonoton then Axis vec
-  else error ("Error in Axis.generate called by " ++
-       caller ++ " - vector of elements is not monotonically rising")
+fromVec caller vec = 
+  if isMonoton then Axis vec   
+  else error ("Error in Axis.generate called by " ++ 
+       caller ++ " - vector of elements is not monotonically rising")   
     where isMonoton = SV.all (==True) $ SV.deltaMap (\ x1 x2 -> x2 > x1) vec
-
-
-type Axes dim vec a = Dim.Data dim (Axis vec a)
-
-create ::
+          
+createSystem :: 
   (Dim.Dimensions dim,
    Ord a,
    SV.Zipper vec,
    SV.Storage vec a,
    SV.Storage vec Bool,
    SV.Singleton vec) =>
-            Caller -> [vec a] -> Axes dim vec a
-create caller xs = Dim.fromList (caller ++">genAxes")
-                    $ map (fromVec (caller ++ ">genAxes")) xs
+            Caller -> [vec a] -> System dim vec a
+createSystem caller xs = Dim.fromList (caller ++">create")  
+                    $ map (fromVec (caller ++ ">create")) xs
 
 
-findIndex ::
+findIndex :: 
   (SV.Storage vec a, SV.Find vec)=>
   (a -> Bool) -> Axis vec a -> Maybe Idx
 findIndex f (Axis vec) = fmap Idx $ SV.findIndex f vec
 
 
-unsafeLookup ::
-  SV.UnsafeLookup vec a =>
+unsafeLookup :: 
+  SV.UnsafeLookup vec a => 
   Axis vec a -> Idx -> a
-unsafeLookup (Axis axis) (Idx idx) = SV.unsafeLookup axis idx
+unsafeLookup (Axis axis) (Idx idx) = SV.unsafeLookup axis idx 
 
-findRightInterpolationIndex ::
+findRightInterpolationIndex :: 
   (SV.Storage vec a, SV.Find vec, Ord a, SV.Length vec) =>
   Axis vec a -> a -> Idx
 findRightInterpolationIndex axis x = rightIndex
-  where
+  where 
     idx = findIndex (>x) axis
     rightIndex = case idx of
-      Just (Idx idx) -> if idx==0 then Idx 1 else Idx idx
+      Just (Idx idx) -> if idx==0 then Idx 1 else Idx idx 
       Nothing   -> Idx $ (len axis)-1
 
 
-getSupportPoints ::
+getSupportPoints :: 
   (Ord a,
    SV.Storage vec a,
    SV.Length vec,
@@ -83,21 +93,21 @@ getSupportPoints axis x = ((leftIndex,rightIndex),
   where rightIndex = findRightInterpolationIndex axis x
         leftIndex = indexAdd rightIndex (-1)
 
-generate::
+vector:: 
   (SV.Walker vec,SV.Storage vec (Dim.Data dim a),
    SV.Singleton vec,
    SV.Storage vec [a],
-   SV.Storage vec a,
+   SV.Storage vec a, 
    SV.Storage (Axis vec) (vec [a]),
-   SV.FromList (Axis vec),
-   SV.Walker (Axis vec),
+   SV.FromList (Axis vec), 
+   SV.Walker (Axis vec), 
    SV.Storage (Axis vec) a) =>
-  Axes dim vec a ->
+  System dim vec a -> 
   vec (Dim.Data dim a)
-generate axes = SV.map Dim.Data $ g axes
-  where
+vector axes = SV.map Dim.Data $ g axes   
+  where   
     g (Dim.Data [Axis axis]) = SV.map (\x -> [x]) $ axis
-    g (Dim.Data axes) =
+    g (Dim.Data axes) = 
       SV.concat $ SV.toList $ SV.map (\x -> SV.map (\xs -> x:xs) vector) axis
       where axis = head axes
             vector = g $ (Dim.Data $ tail axes)
---
+++ src/EFA/Signal/Map/Cube.hs
@@ -0,0 +1,181 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module EFA.Signal.Map.Cube where
+
+import EFA.Utility(Caller)
+
+import qualified EFA.Equation.Arithmetic as Arith
+import qualified EFA.Signal.Vector as SV
+import qualified EFA.Signal.Map.Dimension as Dim
+import EFA.Signal.Map.Dimension(Dim2)
+import qualified EFA.Signal.Map.Coordinate as Coord
+import EFA.Signal.Map.Coordinate (Axis,System)
+
+import EFA.Signal.Interp as Interp
+
+import qualified Prelude as P
+import Prelude hiding (zipWith, map, foldl)
+import qualified Data.Vector as V 
+import Data.Maybe(fromMaybe) 
+
+import EFA.Utility.Trace(mytrace)
+
+
+data Cube dim vec a b = Cube {
+  getAxes :: System dim vec a, 
+  getVector :: vec b } deriving Show
+
+newtype LinIdx = LinIdx {getInt:: Int} deriving Show
+
+toLinear :: 
+ (SV.Storage vec a, SV.Length vec)=>  
+  System dim vec a -> Coord.DimIdx dim -> LinIdx
+toLinear (Dim.Data xs) (Dim.Data indices) = LinIdx $
+  P.foldl (+) (0) $ P.zipWith (*) 
+  (P.map Coord.getInt indices) dimensionMultiplicators
+  where dimensionMultiplicators = (init $ P.map Coord.len xs) ++ [1]
+        
+lookupLin ::
+  (SV.LookupMaybe vec b,
+  Show (vec b)) =>  
+  Caller -> Cube dim vec a b -> LinIdx -> b
+lookupLin caller (Cube _ vec) (LinIdx idx) = fromMaybe (error m) $ SV.lookupMaybe vec idx
+  where m = "Error in LookupLinear called by " ++ caller ++
+            " - linear Index out of Bounds - Index : " ++ show idx ++"- Vector: "++ show vec  
+
+lookUp :: 
+  (SV.LookupMaybe vec b, 
+   SV.Storage vec a,Show (vec b), 
+   SV.Length vec) =>
+  Caller -> Coord.DimIdx dim -> Cube dim vec a b -> b 
+lookUp caller idx ortho@(Cube axes _) = lookupLin (caller ++">lookUp") ortho index
+  where index = toLinear axes idx
+
+checkVector :: 
+  (SV.Storage vec b, SV.Length vec,SV.Storage vec a) =>
+  System dim vec a -> vec b -> Bool
+checkVector (Dim.Data axes) vec =  SV.length vec == numberElements
+  where numberElements = P.foldl (*) (1) (fmap Coord.len axes)
+    
+
+create :: 
+  (Ord a,
+   SV.Zipper vec,
+   SV.Storage vec Bool,
+   SV.Storage vec a,
+   SV.Singleton vec,
+   SV.Storage vec b, 
+   SV.Length vec,
+   Dim.Dimensions dim
+  ) =>
+  Caller -> [vec a] -> vec b -> Cube dim vec a b
+create caller xs vec = 
+  let axes = Coord.createSystem (caller++">genCube") xs
+  in if checkVector axes vec
+  then Cube axes vec
+       else error ("Error in Cube.genCube called by " ++ 
+            caller ++ " - Vector doesn't contain the right number of elements")
+
+map :: 
+  (SV.Walker vec,
+   SV.Storage vec c,
+   SV.Storage vec b) => 
+  (b -> c) -> Cube dim vec a b -> Cube dim vec a c
+map f (Cube axes vec) = (Cube axes $ SV.map f vec)
+
+
+mapWithCoordinates :: 
+ (SV.Walker vec,
+  SV.Storage vec c,
+  SV.Zipper vec,
+  SV.Storage vec b,
+  SV.Storage vec (Dim.Data dim a),
+  SV.Walker (Coord.Axis vec),
+  SV.Storage (Coord.Axis vec) a,
+  SV.Storage (Coord.Axis vec) (vec [a]),
+  SV.Storage vec a,
+  SV.Storage vec [a],
+  SV.Singleton vec,
+  SV.FromList (Coord.Axis vec),
+  SV.Storage vec (Dim.Data dim a, b)) =>
+ (Dim.Data dim a -> b -> c) -> Cube dim vec a b -> Cube dim vec a c
+mapWithCoordinates f (Cube axes vec)  = (Cube axes $ SV.map (\(x,y) -> f x y) $ 
+                                  SV.zip (Coord.vector axes) vec)
+
+foldl ::(SV.Walker vec, SV.Storage vec b)=>
+ (acc -> b -> acc) -> acc -> Cube dim vec a b -> acc
+foldl f acc (Cube _ vec) = SV.foldl f acc vec 
+
+foldlWithCoordinates :: 
+  (SV.Walker vec,
+   SV.Zipper vec,
+   SV.Storage vec b,
+   SV.Storage vec (Dim.Data dim a),
+   SV.Storage vec (Dim.Data dim a, b),
+   SV.Walker (Coord.Axis vec),
+   SV.Storage (Coord.Axis vec) a,
+   SV.Storage (Coord.Axis vec) (vec [a]),
+   SV.Storage vec a,
+   SV.Storage vec [a],
+   SV.Singleton vec,
+   SV.FromList (Coord.Axis vec)) =>
+  (acc -> (Dim.Data dim a, b) -> acc) -> acc -> Cube dim vec a b -> acc
+foldlWithCoordinates f acc (Cube axes vec) = SV.foldl f acc $ SV.zip (Coord.vector axes) vec 
+
+zipWith :: 
+  (SV.Zipper vec,
+   SV.Storage vec d,
+   SV.Storage vec c,
+   SV.Storage vec b, 
+   Eq (System dim vec a)) =>
+  Caller -> 
+  (b -> c -> d) -> 
+  Cube dim vec a b -> 
+  Cube dim vec a c -> 
+  Cube dim vec a d
+zipWith caller f (Cube axes vec) (Cube axes1 vec1) = 
+  if axes == axes1 then Cube axes $ SV.zipWith f vec vec1 
+                   else error ("Error in Cube.zipWith called by " ++ caller ++
+                               "- Axis differ")    
+
+
+-- | Removes the first Dimension
+getSubCube ::  
+  (SV.Storage vec b, SV.Slice vec, 
+   SV.Storage vec a, SV.Length vec) =>
+  Caller ->
+  Cube dim vec a b -> 
+  Coord.Idx -> Cube (Dim.SubDim dim) vec a b
+getSubCube caller (Cube axes vec) (Coord.Idx idx) = Cube (Dim.dropFirst (caller++">getSubCube") axes) subVec
+  where subVec = SV.slice startIdx l vec
+        startIdx = mytrace 0 "getSubCube" "startIdx" $ idx*l
+        l = mytrace 0 "getSubCube" "l" $ Coord.len $ Dim.getFirst (caller++">getSubCube") axes
+
+interpolate :: 
+  (Ord a,Arith.Constant b,Num b,SV.LookupMaybe vec b,
+   SV.UnsafeLookup vec a,Show (vec b),(Show (vec a)),
+   SV.Storage vec a,
+   SV.Length vec,
+   SV.Find vec, 
+   SV.Storage vec b, SV.Slice vec) =>
+  Caller ->  
+  ((a,a) -> (b,b) -> a -> Interp.Val b) -> 
+  Cube dim vec a b -> 
+  (Dim.Data dim a) ->   
+  Interp.Val b
+interpolate caller interpFunction ortho coordinates = Interp.combine3 y1 y2 y
+  where 
+    newCaller = (caller ++ ">Cube.interpolate")
+    axis = mytrace 0 "interpolate" "axis" $ Dim.getFirst newCaller $ 
+           getAxes $ mytrace 0 "interpolate" "ortho" $ ortho
+    subCoordinates = Dim.dropFirst newCaller coordinates
+    x = Dim.getFirst newCaller $ coordinates
+    ((idx1,idx2),(x1,x2)) = Coord.getSupportPoints axis x
+    f idx = interpolate newCaller interpFunction (getSubCube newCaller ortho idx) subCoordinates
+    (y1,y2) = if Dim.len coordinates >=2 then (f idx1, f idx2) 
+              else (Interp.Inter $ lookUp newCaller (Dim.Data [idx1]) ortho,
+                    Interp.Inter $ lookUp newCaller (Dim.Data [idx2]) ortho)
+    y = interpFunction (x1,x2) (Interp.unpack y1,Interp.unpack y2) x
+
+    
+
